b4195fc5bd5f9146f3a64d274e14cf340baf9232
Itay Marder <marderit@gmail.com>
2022-03-01 15:44:42 +0200

    feat: added req.user types

--- gateway-service/src/utils/express/auth.ts
+++ gateway-service/src/utils/express/auth.ts
@@ -26,3 +26,29 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
     if (['/isAlive', '/isalive', '/health'].includes(req.path)) return next();
     return passport.authenticate('jwt', { session: false })(req, res, next);
 };
+
+interface ShragaUser {
+    id: string;
+    adfsId: string;
+    genesisId: string;
+    name: { firstName: string; lastName: string };
+    displayName: string;
+    provider: 'Genesis' | string;
+    entityType: string;
+    unit: string;
+    dischargeDay: string;
+    rank: string;
+    job: string;
+    phoneNumbers: string[];
+    address: string;
+    photo: any;
+    RelayState: string;
+}
+
+declare global {
+    // These declaration are merged into express's Request type
+    // this extends @types/passport which extends @types/express
+    namespace Express {
+        export interface User extends ShragaUser {}
+    }
+}
