d7e08b04b028859ce5f3ebd871bdc7eccb9f9dca
Itay Marder <marderit@gmail.com>
2022-03-31 13:40:14 +0300

    fix: categories mock

--- frontend-service/src/mocks/categories.ts
+++ frontend-service/src/mocks/categories.ts
@@ -9,36 +9,43 @@ const mockCategories = (mock: MockAdapter) => {
                 _id: '61e3d8384d51a83e87e83c74',
                 name: 'pepole',
                 displayName: 'אנשים',
+                color: '#B80000',
             },
             {
                 _id: '61e3d8384d51a83e87e83c75',
                 name: 'flights',
                 displayName: 'טיסות',
+                color: '#E65100',
             },
             {
                 _id: '61e3d8384d51a83e87e83c76',
                 name: 'hotels',
                 displayName: 'מלונות',
+                color: '#FCDC00',
             },
             {
                 _id: '61e3d8384d51a83e87e83c77',
                 name: 'money',
                 displayName: 'כסף',
+                color: '#F78DA7',
             },
             {
                 _id: '61e3d8384d51a83e87e83c78',
                 name: 'things',
                 displayName: 'דברים',
+                color: '#7B1FA2',
             },
             {
                 _id: '61e3d8384d51a83e87e83c79',
                 name: 'communcation',
                 displayName: 'תקשורת',
+                color: '#0D47A1',
             },
             {
                 _id: '61e3dee74d51a83e87e83c7b',
                 name: 'trips',
                 displayName: 'טיולים',
+                color: '#B3E5FC',
             },
         ],
     ]);
