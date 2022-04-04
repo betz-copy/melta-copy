fbe13b0077b46d503d72c61ce99fba962501d246
Itay Marder <marderit@gmail.com>
2022-04-04 09:32:52 +0300

    feat: mongo rs with one container

--- scripts/setup_mongo.sh
+++ scripts/setup_mongo.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
-MONGODB1='mongo1:27017'
-MONGODB2='mongo2:27017'
-MONGODB3='mongo3:27017' 
+MONGODB='mongo:27017'
 
 sleep 10
 
-mongo --host "${MONGODB1}" <<EOF
+mongo --host "${MONGODB}" <<EOF
   const cfg = {
     "_id": "rs0",
     "version": 1,
     "members": [
       {
         "_id": 0,
-        "host": "${MONGODB1}",
+        "host": "${MONGODB}",
         "priority": 2
-      },
-      {
-        "_id": 1,
-        "host": "${MONGODB2}",
-        "priority": 0
-      },
-      {
-        "_id": 2,
-        "host": "${MONGODB3}",
-        "priority": 0
       }
     ]
   };
