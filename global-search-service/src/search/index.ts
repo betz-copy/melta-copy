54ac52fa2b911eb870eb4ffc0e5034445262979c
Dor Weitman <dorweitman@gmail.com>
2022-05-12 16:37:23 +0300

    chore: add helmfile

--- global-search-service/src/search/index.ts
+++ global-search-service/src/search/index.ts
@@ -3,7 +3,7 @@ import Neo4jClient from '../utils/neo4j';
 import RedisClient from '../utils/redis';
 import getEntityTemplates from './templates';
 
-const { neo4j } = config;
+const { neo4j, redis } = config;
 
 const createIndex = async (indexName: string, labels: string[], properties: string[]) => {
     const createFullTextIndexCommand = `
@@ -22,27 +22,27 @@ const dropIndex = (indexName: string) => {
 
 const updateIndexWithTemplates = async (labels: string[], properties: string[]) => {
     const redisClient = RedisClient.getClient();
-    const latestIndex = await redisClient.get('latestIndex');
+    const latestIndex = await redisClient.get(redis.globalSearchKeyName);
 
     const [primarySearchIndex, secondarySearchIndex] = neo4j.globalSearchIndexes;
 
     if (!latestIndex) {
         await createIndex(primarySearchIndex, labels, properties);
-        await redisClient.set('latestIndex', primarySearchIndex);
+        await redisClient.set(redis.globalSearchKeyName, primarySearchIndex);
 
         return;
     }
 
     if (latestIndex === primarySearchIndex) {
         await createIndex(secondarySearchIndex, labels, properties);
-        await redisClient.set('latestIndex', secondarySearchIndex);
+        await redisClient.set(redis.globalSearchKeyName, secondarySearchIndex);
         await dropIndex(primarySearchIndex);
 
         return;
     }
 
     await createIndex(primarySearchIndex, labels, properties);
-    await redisClient.set('latestIndex', primarySearchIndex);
+    await redisClient.set(redis.globalSearchKeyName, primarySearchIndex);
     await dropIndex(secondarySearchIndex);
 };
 
