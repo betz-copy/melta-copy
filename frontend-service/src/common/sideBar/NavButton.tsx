478c6b040fe0283a4b59e1afe321568db801abed
Yahalom <yahalom8848@gmail.com>
2022-05-22 16:55:30 +0300

    fix: remove unused import

--- frontend-service/src/common/sideBar/NavButton.tsx
+++ frontend-service/src/common/sideBar/NavButton.tsx
@@ -1,4 +1,4 @@
-import React, { isValidElement } from 'react';
+import React from 'react';
 import { ListItemButton, Tooltip } from '@mui/material';
 import i18next from 'i18next';
 import { StyledLink, StyledListItemText } from './NavBar.styled';
@@ -28,7 +28,7 @@ const NavButton: React.FC<{ to: string; isDrawerOpen: boolean; text: string; dis
                                 backgroundColor: isActive ? 'rgba(255, 255, 255, 0.17)' : 'transparent',
                             }}
                         >
-                            children
+                            {children}
                             {isDrawerOpen && <StyledListItemText primary={text} sx={{ color: 'white' }} />}
                         </ListItemButton>
                     </span>
