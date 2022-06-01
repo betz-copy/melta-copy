ca6750c967da152e77f9a45cdfb926b24df97373
Itay Marder <marderit@gmail.com>
2022-06-01 11:46:58 +0000

    feat: validate relationship instances

--- relationship-template-service/src/express/relationshipTemplate/manager.ts
+++ relationship-template-service/src/express/relationshipTemplate/manager.ts
@@ -1,37 +1,15 @@
 import { FilterQuery, Document } from 'mongoose';
-import { AxiosError } from 'axios';
 import RelationshipTemplateModel from './model';
 import { IRelationshipTemplate } from './interface';
 import { ServiceError } from '../error';
-import { escapeRegExp, trycatch } from '../../utils';
-import { getEntityTemplatebyId } from '../../entityTemplateManager';
+import { escapeRegExp } from '../../utils';
 
 export class RelationshipTemplateManager {
-    static async throw403IfEntityDoesntExist(entityId: string, errorMessage: string) {
-        const { err: getEntityErr } = await trycatch(() => getEntityTemplatebyId(entityId));
-        if (getEntityErr) {
-            if ((getEntityErr as AxiosError).response?.status === 404) {
-                throw new ServiceError(403, errorMessage);
-            }
-            throw getEntityErr;
-        }
-    }
-
     static getTemplateById(templateId: string) {
         return RelationshipTemplateModel.findById(templateId).orFail(new ServiceError(404, 'Relationship Template not found')).lean().exec();
     }
 
     static async updateTemplateById(templateId: string, updatedFields: Partial<IRelationshipTemplate>) {
-        if (updatedFields.sourceEntityId) {
-            await RelationshipTemplateManager.throw403IfEntityDoesntExist(updatedFields.sourceEntityId, 'source entity of relation doesnt exist');
-        }
-        if (updatedFields.destinationEntityId) {
-            await RelationshipTemplateManager.throw403IfEntityDoesntExist(
-                updatedFields.destinationEntityId,
-                'destination entity of relation doesnt exist',
-            );
-        }
-
         return RelationshipTemplateModel.findByIdAndUpdate(templateId, updatedFields, { new: true })
             .orFail(new ServiceError(404, 'Relationship Template not found'))
             .lean()
@@ -43,12 +21,6 @@ export class RelationshipTemplateManager {
     }
 
     static async createTemplate(relationshipTemplate: IRelationshipTemplate) {
-        await RelationshipTemplateManager.throw403IfEntityDoesntExist(relationshipTemplate.sourceEntityId, 'source entity of relation doesnt exist');
-        await RelationshipTemplateManager.throw403IfEntityDoesntExist(
-            relationshipTemplate.destinationEntityId,
-            'destination entity of relation doesnt exist',
-        );
-
         return RelationshipTemplateModel.create(relationshipTemplate);
     }
 
