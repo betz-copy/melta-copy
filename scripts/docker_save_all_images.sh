524c6675df5475373fbd73c4519ceef4e9936178
Itay Marder <marderit@gmail.com>
2022-06-13 15:54:00 +0300

    feat: better save images script

--- scripts/docker_save_all_images.sh
+++ scripts/docker_save_all_images.sh
@@ -7,9 +7,9 @@ printf "\nForbidden images:\n"
 cat ./scripts/forbidden-images.txt
 
 printf "\nImages list:\n"
-cat docker-compose.yml | grep "image:" | awk '{print $2}' | grep -v -F -f ./scripts/forbidden-images.txt
+cat docker-compose.yml docker-compose.dev.yml | grep "image:" | awk '{print $2}' | grep -v -F -f ./scripts/forbidden-images.txt
 
-docker save -o images.tar `cat docker-compose.yml | grep "image:" | awk '{print $2}' | grep -v -F -f ./scripts/forbidden-images.txt`
+docker save -o images.tar `cat docker-compose.yml docker-compose.dev.yml | grep "image:" | awk '{print $2}' | grep -v -F -f ./scripts/forbidden-images.txt`
 
 printf "\nGenerated file:\n" 
 du images.tar -h
