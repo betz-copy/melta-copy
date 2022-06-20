ac11820a3a43f3815529909eb2821da6231d26ff
Itay Marder <marderit@gmail.com>
2022-06-20 14:35:02 +0000

    fix: removed unused flex properties

--- frontend-service/src/common/TemplatesTablesPage/TemplateTablesHeadline.tsx
+++ frontend-service/src/common/TemplatesTablesPage/TemplateTablesHeadline.tsx
@@ -59,8 +59,8 @@ const TemplateTablesHeadline: React.FC<{
     return (
         <Grid container spacing={2}>
             <Grid item xs={12} container justifyContent="space-between" alignItems="center">
-                <Grid item xs={6}>
-                    <Grid container spacing={1} alignItems="center">
+                <Grid item>
+                    <Grid container spacing={1}>
                         <Grid item flexGrow={1}>
                             <GlobalSearch onSearch={onSearch} />
                         </Grid>
@@ -78,8 +78,8 @@ const TemplateTablesHeadline: React.FC<{
                         </Grid>
                     </Grid>
                 </Grid>
-                <Grid item xs={6}>
-                    <Grid container spacing={1} justifyContent="right" alignItems="center">
+                <Grid item>
+                    <Grid container spacing={1}>
                         <Grid item>
                             <IconButton style={{ background: 'white', borderRadius: '7px' }} onClick={onExcelExport}>
                                 <DonwloadIcon color="primary" />
