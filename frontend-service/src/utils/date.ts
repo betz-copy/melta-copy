046653c6d5faf91165edb24beb7916938fabc7f7
nadav beladev <nadybeladev@gmail.com>
2022-10-31 16:32:07 +0200

    fix: date format

--- frontend-service/src/utils/date.ts
+++ frontend-service/src/utils/date.ts
@@ -1,23 +1,11 @@
+import { format } from 'date-fns';
+
 export const getShortDate = (date: Date) => {
-    return new Date(date).toLocaleString('en-GB', {
-        day: '2-digit',
-        month: '2-digit',
-        year: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-    });
+    return format(new Date(date), 'dd/MM/yy, HH:mm');
 };
 export const getLongDate = (date: Date) => {
-    return new Date(date).toLocaleString('en-GB', {
-        timeZone: 'Israel',
-        day: '2-digit',
-        month: '2-digit',
-        year: 'numeric',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-    });
+    return format(new Date(date), 'dd/MM/yyyy, HH:mm:ss');
 };
 export const getDateWithoutTime = (date: Date) => {
-    return new Date(date).toLocaleString('en-GB', { timeZone: 'Israel' });
+    return format(new Date(date), 'dd/MM/yyyy');
 };
