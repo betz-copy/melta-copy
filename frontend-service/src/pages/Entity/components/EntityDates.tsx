c9e2107ab8dbe0b5de3c74a4966a1bdf29c97456
noam7700 <noam7700@gmail.com>
2022-11-29 15:57:46 +0000

    refactor: use getLongDate in entity page

--- frontend-service/src/pages/Entity/components/EntityDates.tsx
+++ frontend-service/src/pages/Entity/components/EntityDates.tsx
@@ -3,17 +3,18 @@ import { Grid, Typography } from '@mui/material';
 import i18next from 'i18next';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../../store';
+import { getLongDate } from '../../../utils/date';
 
 const EntityDates: React.FC<{ createdAt: string; updatedAt: string }> = ({ createdAt, updatedAt }) => {
     const darkMode = useSelector((state: RootState) => state.darkMode);
 
     return (
         <Grid item container justifyContent="space-around">
-            <Typography color={darkMode ? '#cecece' : 'gray'}>{`${i18next.t('entityPage.createdAt')}: ${new Date(createdAt).toLocaleString(
-                'en-uk',
+            <Typography color={darkMode ? '#cecece' : 'gray'}>{`${i18next.t('entityPage.createdAt')}: ${getLongDate(
+                new Date(createdAt),
             )}`}</Typography>
-            <Typography color={darkMode ? '#cecece' : 'gray'}>{`${i18next.t('entityPage.updatedAt')}: ${new Date(updatedAt).toLocaleString(
-                'en-uk',
+            <Typography color={darkMode ? '#cecece' : 'gray'}>{`${i18next.t('entityPage.updatedAt')}: ${getLongDate(
+                new Date(updatedAt),
             )}`}</Typography>
         </Grid>
     );
