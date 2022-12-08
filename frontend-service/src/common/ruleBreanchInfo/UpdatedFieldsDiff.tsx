246505b19769275ce50af730d6bbcab36e81a4b4
Yahalom <yahalom8848@gmail.com>
2022-12-08 15:49:18 +0200

    fix: darkmode diff view

--- frontend-service/src/common/ruleBreanchInfo/UpdatedFieldsDiff.tsx
+++ frontend-service/src/common/ruleBreanchInfo/UpdatedFieldsDiff.tsx
@@ -3,10 +3,12 @@ import ReactDiffViewer from 'react-diff-viewer';
 import i18next from 'i18next';
 import { Typography } from '@mui/material';
 import pickBy from 'lodash.pickby';
+import { useSelector } from 'react-redux';
 import { IUpdateEntityMetadataPopulated } from '../../interfaces/ruleBreaches/actionMetadata';
 import { IMongoEntityTemplatePopulated } from '../../interfaces/entityTemplates';
 import { formatToString } from '../EntityProperties';
 import { getFileName } from '../../utils/getFileName';
+import { RootState } from '../../store';
 
 const getEntityPropertyString = (value: any, type: 'string' | 'number' | 'boolean', format: string | undefined, oldValue: any) => {
     if (value === null || value === undefined) {
@@ -50,6 +52,8 @@ export const UpdatedFieldsDiff: React.FC<{
     const { entity, before, updatedFields } = actionMetadata;
     const oldProperties = before ?? entity?.properties;
 
+    const darkMode = useSelector((state: RootState) => state.darkMode);
+
     const newPropertiesWithNulls = { ...oldProperties, ...updatedFields };
     // updatedFields specifies fields to remove w/ nulls. but shouldnt be in the IEntity properties
     const newProperties = pickBy(newPropertiesWithNulls, (property) => property !== null);
@@ -77,6 +81,7 @@ export const UpdatedFieldsDiff: React.FC<{
                     {i18next.t('ruleBreachInfo.updateEntityActionInfo.entityAfter')}
                 </Typography>
             }
+            useDarkTheme={darkMode}
             disableWordDiff
         />
     );
