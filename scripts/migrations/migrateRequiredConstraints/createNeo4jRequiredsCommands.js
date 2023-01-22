91d2391e6828db0d44703823ad07133d620bd841
noam7700 <noam7700@gitlab.com>
2023-01-22 09:23:13 +0200

    fix: fix migrate required indexes script

--- scripts/migrations/migrateRequiredConstraints/createNeo4jRequiredsCommands.js
+++ scripts/migrations/migrateRequiredConstraints/createNeo4jRequiredsCommands.js
@@ -1,10 +1,11 @@
 const templates = require('./templates.json');
 
 const createRequiredCommandsForEachTemplate = templates.map(template => {
+    const templateId = template._id.$oid;
     const createRequiredCommands = template.properties.required.map(requiredProp => {
-        const constraintName = `requiredConstraint_${template._id}_${requiredProp}`;
+        const constraintName = `requiredConstraint_${templateId}_${requiredProp}`;
 
-        return `CREATE CONSTRAINT \`${constraintName}\` ON (n:\`${template._id}\`) ASSERT exists(n.${requiredProp})`
+        return `CREATE CONSTRAINT \`${constraintName}\` ON (n:\`${templateId}\`) ASSERT exists(n.${requiredProp})`
     });
     return createRequiredCommands.join(';\n');
 }).join(';\n\n');
