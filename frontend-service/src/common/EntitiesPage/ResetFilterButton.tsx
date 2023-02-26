9f589315269710bd7f7dc2aa359b4a7cc02a8fc2
noam7700 <noam7700@gitlab.com>
2023-02-26 17:03:08 +0200

    fix: missed type error

--- frontend-service/src/common/EntitiesPage/ResetFilterButton.tsx
+++ frontend-service/src/common/EntitiesPage/ResetFilterButton.tsx
@@ -3,10 +3,9 @@ import { FilterListOffOutlined } from '@mui/icons-material';
 import i18next from 'i18next';
 import IconButtonWithPopoverText from '../IconButtonWithPopover';
 import { EntitiesTableOfTemplateRef } from '../EntitiesTableOfTemplate';
-import { IEntity } from '../../interfaces/entities';
 
 const ResetFilterButton: React.FC<{
-    entitiesTableRef: React.RefObject<EntitiesTableOfTemplateRef<IEntity>>;
+    entitiesTableRef: React.RefObject<EntitiesTableOfTemplateRef<unknown>>;
 }> = ({ entitiesTableRef }) => {
     return (
         <IconButtonWithPopoverText
