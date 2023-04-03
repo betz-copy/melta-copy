bce3bec18c2a5b2b74a75c0dbd2b2047ab7aaff4
michal_lev <michallev231@gmail.com>
2023-04-03 13:51:41 +0300

    fix: can see text in dark mode

--- frontend-service/src/TourWrapper.tsx
+++ frontend-service/src/TourWrapper.tsx
@@ -62,6 +62,7 @@ const TourWrapper: React.FC = ({ children }) => {
                     ...base,
                     borderRadius: 10,
                     marginTop: 10,
+                    color: '#000000',
                 }),
                 close: (base) => ({ ...base, right: 8, left: 'auto', top: 8, width: '12px', height: '12px' }),
                 arrow: (base) => ({ ...base, height: '18px', width: '24px' }),
