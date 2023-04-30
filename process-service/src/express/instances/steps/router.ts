d2dd7f842e2b4ab4e904a4514af30e0a14c7a620
nadav <nadybeladev@gmail.com>
2023-04-30 13:56:13 +0300

    feat: add get step instance route

--- process-service/src/express/instances/steps/router.ts
+++ process-service/src/express/instances/steps/router.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
 import { wrapController, wrapMiddleware } from '../../../utils/express';
 import ValidateRequest from '../../../utils/joi';
-import { updateStepPropertiesSchema, updateStepStatusSchema } from './validator.schema';
+import {
+    getStepByIdRequestSchema,
+    getTemplateByInstanceIdRequestSchema,
+    updateStepPropertiesSchema,
+    updateStepStatusSchema,
+} from './validator.schema';
 import StepInstanceController from './controller';
 import validateStepInstance from './validator.template';
 
 const StepInstanceRouter: Router = Router();
+
+StepInstanceRouter.get('/:id', ValidateRequest(getStepByIdRequestSchema), wrapController(StepInstanceController.getStepById));
+StepInstanceRouter.get(
+    '/:id/step-template',
+    ValidateRequest(getTemplateByInstanceIdRequestSchema),
+    wrapController(StepInstanceController.getStepTemplateByStepInstanceId),
+);
 
 StepInstanceRouter.patch(
     '/:id/properties',
@@ -13,6 +25,7 @@ StepInstanceRouter.patch(
     wrapMiddleware(validateStepInstance),
     wrapController(StepInstanceController.updateStepProperties),
 );
+
 StepInstanceRouter.patch('/:id/status', ValidateRequest(updateStepStatusSchema), wrapController(StepInstanceController.updateStepStatus));
 
 export default StepInstanceRouter;
