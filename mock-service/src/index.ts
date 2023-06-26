f063616a8abd3fa2d71428976afb32ae0fba726b
noam7700 <noam7700@gitlab.com>
2023-06-26 14:43:43 +0300

    feat: mv to node:16 to restart + log better
    no need try catch to console.log, in node 16 it logs by itself pretty good.

--- mock-service/src/index.ts
+++ mock-service/src/index.ts
@@ -18,100 +18,96 @@ import { createProcessInstances } from './processInstances';
 import { isStorageServiceAlive, uploadFile } from './storageService';
 
 const main = async () => {
-    try {
-        console.log(`Mock started ${JSON.stringify(config, null, 4)}`);
+    console.log(`Mock started ${JSON.stringify(config, null, 4)}`);
 
-        const { err: entityTemplateManagerAliveErr } = await isEntityTemplateManagerAlive();
-        if (entityTemplateManagerAliveErr) {
-            console.log('Entity Template Manager is not alive');
-            throw entityTemplateManagerAliveErr;
-        }
-
-        const { err: processServiceAliveErr } = await isProcessServiceAlive();
-        if (processServiceAliveErr) {
-            console.log('Process Service is not alive');
-            throw processServiceAliveErr;
-        }
-
-        const { err: relationshipTemplateManagerAliveErr } = await isRelationshipTemplateManagerAlive();
-        if (relationshipTemplateManagerAliveErr) {
-            console.log('Relationship Template Manager is not alive');
-            throw relationshipTemplateManagerAliveErr;
-        }
-
-        const { err: permissionsApiAliveErr } = await isPermissionsApiAlive();
-        if (permissionsApiAliveErr) {
-            console.log('Permissions API is not alive');
-            throw permissionsApiAliveErr;
-        }
-
-        const { err: instanceManagerAliveErr } = await isInstanceManagerAlive();
-        if (instanceManagerAliveErr) {
-            console.log('Instance Manager is not alive');
-            throw instanceManagerAliveErr;
-        }
-
-        const { err: storageServiceAliveErr } = await isStorageServiceAlive();
-        if (storageServiceAliveErr) {
-            console.log('Storage Service is not alive');
-            throw storageServiceAliveErr;
-        }
-
-        if ((await getCategories()).length !== 0) {
-            console.log('DB not empty');
-            return;
-        }
-
-        console.log('All services alive!');
-
-        const seed = config.seed ?? Math.floor(Math.random() * 1000);
-        console.log(`Picked seed ${seed}`);
-
-        const chance = new Chance(seed);
-        JSONSchemaFaker.option({ random: () => chance.floating({ min: 0, max: 0.9999, fixed: 4 }) });
-
-        console.log('Creating categories');
-
-        const createdCategories = await createCategories(categories);
-
-        console.log('Creating entity templates');
-
-        const createdEntityTemplates = await createEntityTemplates(entityTemplates, createdCategories);
-
-        console.log('Creating relationshipTemplates templates');
-
-        const createdRelationshipTemplates = await createRealtionshipTemplates(relationshipTemplates, createdEntityTemplates);
-
-        console.log('Creating rules');
-
-        await createRules(createdEntityTemplates, createdRelationshipTemplates);
-
-        console.log('Creating permissions');
-
-        await createPermissionsBulk(getPermissionsToCreate(createdCategories));
-
-        console.log('Creating example file');
-
-        const exampleFileId = await uploadFile();
-
-        console.log('Creating entities');
-
-        const createdEntityInstances = await createInstances(createdEntityTemplates, chance, exampleFileId);
-
-        await createRelationshipInstances(createdEntityInstances, createdRelationshipTemplates, chance);
-
-        console.log('Creating process templates');
-
-        const createdProcessTemplates = await createProcessTemplates(processTemplates);
-
-        console.log('Creating process Instances');
-
-        await createProcessInstances(createdProcessTemplates, chance, exampleFileId);
-
-        console.log('Finished');
-    } catch (error) {
-        console.error(error);
+    const { err: entityTemplateManagerAliveErr } = await isEntityTemplateManagerAlive();
+    if (entityTemplateManagerAliveErr) {
+        console.log('Entity Template Manager is not alive');
+        throw entityTemplateManagerAliveErr;
     }
+
+    const { err: processServiceAliveErr } = await isProcessServiceAlive();
+    if (processServiceAliveErr) {
+        console.log('Process Service is not alive');
+        throw processServiceAliveErr;
+    }
+
+    const { err: relationshipTemplateManagerAliveErr } = await isRelationshipTemplateManagerAlive();
+    if (relationshipTemplateManagerAliveErr) {
+        console.log('Relationship Template Manager is not alive');
+        throw relationshipTemplateManagerAliveErr;
+    }
+
+    const { err: permissionsApiAliveErr } = await isPermissionsApiAlive();
+    if (permissionsApiAliveErr) {
+        console.log('Permissions API is not alive');
+        throw permissionsApiAliveErr;
+    }
+
+    const { err: instanceManagerAliveErr } = await isInstanceManagerAlive();
+    if (instanceManagerAliveErr) {
+        console.log('Instance Manager is not alive');
+        throw instanceManagerAliveErr;
+    }
+
+    const { err: storageServiceAliveErr } = await isStorageServiceAlive();
+    if (storageServiceAliveErr) {
+        console.log('Storage Service is not alive');
+        throw storageServiceAliveErr;
+    }
+
+    if ((await getCategories()).length !== 0) {
+        console.log('DB not empty');
+        return;
+    }
+
+    console.log('All services alive!');
+
+    const seed = config.seed ?? Math.floor(Math.random() * 1000);
+    console.log(`Picked seed ${seed}`);
+
+    const chance = new Chance(seed);
+    JSONSchemaFaker.option({ random: () => chance.floating({ min: 0, max: 0.9999, fixed: 4 }) });
+
+    console.log('Creating categories');
+
+    const createdCategories = await createCategories(categories);
+
+    console.log('Creating entity templates');
+
+    const createdEntityTemplates = await createEntityTemplates(entityTemplates, createdCategories);
+
+    console.log('Creating relationshipTemplates templates');
+
+    const createdRelationshipTemplates = await createRealtionshipTemplates(relationshipTemplates, createdEntityTemplates);
+
+    console.log('Creating rules');
+
+    await createRules(createdEntityTemplates, createdRelationshipTemplates);
+
+    console.log('Creating permissions');
+
+    await createPermissionsBulk(getPermissionsToCreate(createdCategories));
+
+    console.log('Creating example file');
+
+    const exampleFileId = await uploadFile();
+
+    console.log('Creating entities');
+
+    const createdEntityInstances = await createInstances(createdEntityTemplates, chance, exampleFileId);
+
+    await createRelationshipInstances(createdEntityInstances, createdRelationshipTemplates, chance);
+
+    console.log('Creating process templates');
+
+    const createdProcessTemplates = await createProcessTemplates(processTemplates);
+
+    console.log('Creating process Instances');
+
+    await createProcessInstances(createdProcessTemplates, chance, exampleFileId);
+
+    console.log('Finished');
 };
 
 main();
