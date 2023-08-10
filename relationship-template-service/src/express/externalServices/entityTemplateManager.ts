0d5e0a8ffb66f4c7af26e9d8a68837a7d8c11344
Michal Lev <michallev231@gmail.com>
2023-08-10 11:55:19 +0000

    feat: Add date alerts

--- relationship-template-service/src/express/externalServices/entityTemplateManager.ts
+++ relationship-template-service/src/express/externalServices/entityTemplateManager.ts
@@ -20,6 +20,7 @@ export interface IEntitySingleProperty {
     enum?: string[];
     pattern?: string;
     patternCustomErrorMessage?: string;
+    dateNotification?: 'day' | 'week' | 'twoWeeks';
 }
 
 export interface IEntityTemplate {
