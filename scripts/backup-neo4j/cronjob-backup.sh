b4da164671558ded38775ee22beee2b6351bfd84
nadav <nadybeladev@gmail.com>
2023-10-10 06:04:44 +0000

    feat: neo4j backups delete old calc in mins
    see changes' comments in guide.txt

--- scripts/backup-neo4j/cronjob-backup.sh
+++ scripts/backup-neo4j/cronjob-backup.sh
@@ -3,7 +3,7 @@
 export JAVA_HOME=/usr/lib/jvm/jdk-11.0.6 # used in neo4j-admin command. need to set because crontab doesnt load enviorments of bash
 
 BACKUPS_DIR=$1 # for example: /data/neo4j-melta/neo4j-enterprise-4.4.18/dumps/daily
-DAYS_TO_DELETE=$2 # for example: 7 (days)
+MINS_TO_DELETE=$2 # for example: 10080 (7 days in mins)
 RSYNC_OTHER_DIRECTORY_PATH=$3 # will skip if not given path. for example: /root/persistent-storage/neo4j-melta/dumps/daily
 
 NEW_BACKUP_DIR="$BACKUPS_DIR/backup-$(date +%FT%H:%M:%S)"
@@ -15,7 +15,7 @@ mkdir $NEW_BACKUP_DIR
 # (&& to ensure deleting old backups only if successfuly created new)
 /data/neo4j-melta/neo4j-enterprise-4.4.18/bin/neo4j-admin backup --backup-dir $NEW_BACKUP_DIR &&
 # mkdir $NEW_BACKUP_DIR/neo4j &&  # fake backup, for debugging
-find "$BACKUPS_DIR" -name "backup-*" -type d -mtime +$DAYS_TO_DELETE -exec rm -r {} +
+find "$BACKUPS_DIR" -name "backup-*" -type d -mtime +$MINS_TO_DELETE -exec rm -r {} +
 
 if [ $RSYNC_OTHER_DIRECTORY_PATH ]
 then
