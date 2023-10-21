a1a8a64b0eb6f1bc6ccc82b7bb37616659fc09fb
noam7700 <noam7700@gmail.com>
2023-10-21 09:59:40 +0000

    chore: fix backups dont run calc in crontab
    - dont run calc in crontab. for example =$(expr 6 \* 24 \* 60 + 60)
    - add middaily backups
    - small syntax+typos

--- scripts/backup-neo4j/cronjob-backup.sh
+++ scripts/backup-neo4j/cronjob-backup.sh
@@ -10,15 +10,15 @@ NEW_BACKUP_DIR="$BACKUPS_DIR/backup-$(date +%FT%H:%M:%S)"
 
 echo "Creating backup of $NEW_BACKUP_DIR"
 
-mkdir $NEW_BACKUP_DIR
+mkdir "$NEW_BACKUP_DIR"
 
 # (&& to ensure deleting old backups only if successfuly created new)
-/data/neo4j-melta/neo4j-enterprise-4.4.18/bin/neo4j-admin backup --backup-dir $NEW_BACKUP_DIR &&
+/data/neo4j-melta/neo4j-enterprise-4.4.18/bin/neo4j-admin backup --backup-dir "$NEW_BACKUP_DIR" &&
 # mkdir $NEW_BACKUP_DIR/neo4j &&  # fake backup, for debugging
 find "$BACKUPS_DIR" -name "backup-*" -type d -mtime +$MINS_TO_DELETE -exec rm -r {} +
 
 if [ $RSYNC_OTHER_DIRECTORY_PATH ]
 then
   echo "mirroring backups to dir named: $RSYNC_OTHER_DIRECTORY_PATH"
-  rsync --ignore-existing --delete-after --recursive $BACKUPS_DIR/ $RSYNC_OTHER_DIRECTORY_PATH
+  rsync --ignore-existing --delete-after --recursive "$BACKUPS_DIR/" "$RSYNC_OTHER_DIRECTORY_PATH"
 fi
