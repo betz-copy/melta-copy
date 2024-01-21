037b46ca73c963a5cb433de28fedbcaa5ff91146
nadav882 <nadybeladev@gmail.com>
2024-01-21 14:29:39 +0200

    feat: popover on cell when double click

--- frontend-service/src/utils/agGrid/Value.tsx
+++ frontend-service/src/utils/agGrid/Value.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { IconButton, Grid } from '@mui/material';
+import { IconButton, Grid, Popover } from '@mui/material';
 import { ColoredEnumChip } from '../../common/ColoredEnumChip';
 
 const Value: React.FC<{
@@ -11,10 +11,22 @@ const Value: React.FC<{
     useDefaultColoredEnum?: boolean;
 }> = ({ hideValue, value, color, isNumberField }) => {
     const [hideField, setHideField] = React.useState(true);
-    const [wrapText, setWrapText] = React.useState(false);
+    const [anchorEl, setAnchorEl] = React.useState<HTMLDivElement | null>(null);
+
     const handleClick = () => {
         setHideField((curr) => !curr);
     };
+
+    const handleDoubleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
 
     let innerContent;
     if (hideValue && hideField) innerContent = <>••••••••</>;
@@ -22,7 +34,7 @@ const Value: React.FC<{
     else innerContent = value;
 
     return (
-        <Grid container justifyContent="space-between" alignItems="center" wrap={wrapText ? 'wrap' : 'nowrap'}>
+        <Grid container justifyContent="space-between" alignItems="center">
             <Grid
                 item
                 sx={{
@@ -30,14 +42,43 @@ const Value: React.FC<{
                     fontSize: '14px',
                     fontWeight: '200',
                     overflow: 'hidden',
-                    whiteSpace: wrapText ? 'normal' : 'nowrap',
+                    whiteSpace: 'nowrap',
                     textOverflow: 'ellipsis',
                     direction: isNumberField ? 'rtl' : undefined,
                 }}
-                onDoubleClick={() => setWrapText(!wrapText)}
+                onDoubleClick={handleDoubleClick}
             >
                 {innerContent}
             </Grid>
+
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+            >
+                <div
+                    style={{
+                        padding: '10px',
+                        maxWidth: '350px',
+                        maxHeight: '200px',
+                        overflow: 'auto',
+                        whiteSpace: 'pre-wrap',
+                        fontWeight: 200,
+                        fontSize: '15px',
+                    }}
+                >
+                    {value}
+                </div>
+            </Popover>
 
             {hideValue && (
                 <Grid item>
