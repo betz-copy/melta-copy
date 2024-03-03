127df7007a7f780fa0af61c4ee69b665b40fc6e5
etrogyam <etrogyam@gmail.com>
2024-03-03 16:36:07 +0200

    fix select year foucus

--- frontend-service/src/common/inputs/JSONSchemaFormik/RjfsDatesWidgets.tsx
+++ frontend-service/src/common/inputs/JSONSchemaFormik/RjfsDatesWidgets.tsx
@@ -1,63 +1,53 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-underscore-dangle */
 import React from 'react';
-// import { styled, TextField, TextFieldProps } from '@mui/material';
+import { styled, TextField, TextFieldProps } from '@mui/material';
 import i18next from 'i18next';
 import { WidgetProps, getDisplayLabel } from '@rjsf/utils';
 import validator from '@rjsf/validator-ajv8';
-import {
-    // dateTimePickerToolbarClasses,
-    LocalizationProvider,
-    MobileDatePicker,
-    MobileDateTimePicker,
-    PickersActionBarAction,
-} from '@mui/x-date-pickers';
-// import heLocale from 'date-fns/locale/he';
+import { LocalizationProvider, MobileDatePicker, MobileDateTimePicker, dateTimePickerToolbarClasses } from '@mui/x-date-pickers';
+import heLocale from 'date-fns/locale/he';
 import format from 'date-fns/format';
 import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { DateTimePickerToolbar } from '@mui/x-date-pickers/DateTimePicker/DateTimePickerToolbar';
+import { BaseToolbarProps } from '@mui/x-date-pickers/internals';
+import { useTheme } from '@mui/material/styles';
 
-// import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-// import { DateTimePickerToolbar } from '@mui/x-date-pickers/DateTimePicker/DateTimePickerToolbar';
-// import { BaseToolbarProps } from '@mui/x-date-pickers/internals';
-
-// const CustomDateTimePickerToolbar = styled(DateTimePickerToolbar)({
-//     [`& .${dateTimePickerToolbarClasses.timeContainer}`]: {
-//         display: 'flex',
-//         flexDirection: 'row-reverse', // support rtl! see issue https://github.com/mui/mui-x/issues/5561
-//     },
-// }) as unknown as (props: JSXElementConstructor<DatePickerToolbarProps<Date>>) => JSX.Element;
+const CustomDateTimePickerToolbar = styled(DateTimePickerToolbar)({
+    [`& .${dateTimePickerToolbarClasses.timeContainer}`]: {
+        display: 'flex',
+        flexDirection: 'row-reverse', // support rtl! see issue https://github.com/mui/mui-x/issues/5561
+    },
+}) as (props: BaseToolbarProps<Date, Date | null>) => JSX.Element;
 
 const getRjfsDateOrDateTimeWidget =
     (dateOrDateTime: 'date' | 'dateTime') =>
     ({
         id,
-        placeholder, // not used
         required,
         readonly,
         disabled,
-        type,
         label,
         value,
         onChange,
         onBlur,
         onFocus,
         autofocus,
-        options,
         schema,
-        uischema,
+        uiSchema,
         rawErrors = [],
         formContext,
         registry,
         color,
-    }: // ...textFieldProps
-    WidgetProps) => {
-        // const _onBlur = ({ target: { value: newValue } }: React.FocusEvent<HTMLInputElement>) => onBlur(id, newValue);
-        // const _onFocus = ({ target: { value: newValue } }: React.FocusEvent<HTMLInputElement>) => onFocus(id, newValue);
+        ...textFieldProps
+    }: WidgetProps) => {
+        const _onBlur = ({ target: { value: newValue } }: React.FocusEvent<HTMLInputElement>) => onBlur(id, newValue);
+        const _onFocus = ({ target: { value: newValue } }: React.FocusEvent<HTMLInputElement>) => onFocus(id, newValue);
 
         const [currDate, setCurrDate] = React.useState<Date | null>(value);
 
         const { rootSchema } = registry;
-        const displayLabel = getDisplayLabel(validator, schema, uischema, rootSchema);
+        const displayLabel = getDisplayLabel(validator, schema, uiSchema, rootSchema);
 
         const MuiDatePicker = dateOrDateTime === 'date' ? MobileDatePicker : MobileDateTimePicker;
         const onChangeDateWidget = (date: Date | null) => {
@@ -75,79 +65,63 @@ const getRjfsDateOrDateTimeWidget =
             return onChange(dateString);
         };
 
-        // const handleOpenDatePicker = () => {
-        //     if (currDate) return;
+        const handleOpenDatePicker = () => {
+            if (currDate) return;
 
-        //     const currentDate = new Date();
-        //     setCurrDate(currentDate);
+            const currentDate = new Date();
+            setCurrDate(currentDate);
 
-        //     if (dateOrDateTime === 'dateTime') {
-        //         return onChange(currentDate.toISOString());
-        //     }
+            if (dateOrDateTime === 'dateTime') {
+                onChange(currentDate.toISOString());
+            }
 
-        //     return onChange(format(currentDate, 'yyyy-MM-dd'));
-        // };
+            onChange(format(currentDate, 'yyyy-MM-dd'));
+        };
+
+        const theme = useTheme();
+        theme.direction = 'ltr';
 
         const onFormChangeFunction = dateOrDateTime === 'date' ? onChangeDateWidget : onChangeDateTimeWidget;
-        // const variant = readonly ? 'standard' : 'outlined';
-        type CustomActionBarAction = PickersActionBarAction & { name: string };
-
+        const variant = readonly ? 'standard' : 'outlined';
         return (
             <LocalizationProvider
                 dateAdapter={AdapterDateFns}
-                // adapterLocale={heLocale}
+                adapterLocale={heLocale}
                 localeText={i18next.t('muiDatePickersLocaleText', { returnObjects: true })}
             >
-                <MuiDatePicker<Date>
+                <MuiDatePicker<Date, Date>
                     value={currDate || null}
                     onChange={(val) => {
                         setCurrDate(val);
                         onFormChangeFunction(val);
                     }}
-                    format={dateOrDateTime === 'date' ? 'dd/MM/yyyy' : 'dd/MM/yyyy HH:mm'}
-                    // componentsProps={{ actionBar: { actions: ['clear', 'cancel', 'accept'] } }}
-                    slotProps={{
-                        actionBar: ({ wrapperVariant }) => ({
-                            actions:
-                                wrapperVariant === 'desktop'
-                                    ? []
-                                    : ([{ name: 'clear' }, { name: 'cancel' }, { name: 'accept' }] as CustomActionBarAction[]),
-                        }),
-                        toolbar: {
-                            toolbarPlaceholder: '__',
-                            toolbarFormat: 'DD / MM / YYYY',
-                            hidden: false,
-                        },
-                        textField: { variant: 'outlined' },
-                    }}
+                    inputFormat={dateOrDateTime === 'date' ? 'dd/MM/yyyy' : 'dd/MM/yyyy HH:mm'}
+                    showToolbar
+                    componentsProps={{ actionBar: { actions: ['clear', 'cancel', 'accept'] } }}
                     label={displayLabel ? label || schema.title : false}
-                    // renderInput={(params) => {
-                    //     return (
-                    //         <TextField
-                    //             {...textFieldProps}
-                    //             color={color as TextFieldProps['color']}
-                    //             {...params}
-                    //             id={id}
-                    //             required={required}
-                    //             onBlur={_onBlur}
-                    //             onFocus={_onFocus}
-                    //             error={rawErrors.length > 0}
-                    //             variant={variant}
-                    //             InputLabelProps={{
-                    //                 shrink: readonly || undefined,
-                    //             }}
-                    //         />
-                    //     );
-                    // }}
+                    renderInput={(params) => (
+                        <TextField
+                            {...textFieldProps}
+                            color={color as TextFieldProps['color']}
+                            {...params}
+                            id={id}
+                            required={required}
+                            onBlur={_onBlur}
+                            onFocus={_onFocus}
+                            error={rawErrors.length > 0}
+                            variant={variant}
+                            InputLabelProps={{
+                                shrink: readonly || undefined,
+                            }}
+                        />
+                    )}
                     readOnly={readonly}
                     disabled={disabled}
-                    autoFocus
-                    // toolbarFormat="dd/MM"
+                    autoFocus={autofocus}
+                    toolbarFormat="dd/MM"
                     ampm={false}
-                    // onOpen={handleOpenDatePicker}
-                    // slots={{ toolbar: CustomDateTimePickerToolbar }}
-                    // ToolbarComponent={CustomDateTimePickerToolbar}
-                    disableHighlightToday
+                    onOpen={handleOpenDatePicker}
+                    ToolbarComponent={CustomDateTimePickerToolbar}
                 />
             </LocalizationProvider>
         );
