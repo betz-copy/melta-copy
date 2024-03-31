3ca3c3ebba517f54fb3054c10eda67a455cc93c6
nadav882 <nadybeladev@gmail.com>
2024-03-31 16:47:26 +0300

    refactor: remove view entity from activity log

--- frontend-service/src/pages/Entity/components/activityLog/index.tsx
+++ frontend-service/src/pages/Entity/components/activityLog/index.tsx
@@ -1,9 +1,7 @@
 import React, { useEffect } from 'react';
 import i18next from 'i18next';
 import { toast } from 'react-toastify';
-import { Box, Divider } from '@mui/material';
-import Tab from '@mui/material/Tab';
-import { TabContext, TabList, TabPanel } from '@mui/lab';
+import { Divider } from '@mui/material';
 import IconButtonWithPopover from '../../../../common/IconButtonWithPopover';
 import { IEntityExpanded } from '../../../../interfaces/entities';
 import { IMongoEntityTemplatePopulated } from '../../../../interfaces/entityTemplates';
@@ -24,7 +22,6 @@ const ActivityLog: React.FC<{ expandedEntity: IEntityExpanded; entityTemplate: I
     useEffect(() => {
         setOpenPopper(false);
     }, [entityId]);
-    const [tab, setTab] = React.useState(i18next.t('entityPage.activityLog.actions') as string);
 
     return (
         <>
@@ -35,71 +32,36 @@ const ActivityLog: React.FC<{ expandedEntity: IEntityExpanded; entityTemplate: I
                 <img src="/icons/history.svg" />
             </IconButtonWithPopover>
             <PopperSidebar open={openPopper} setOpen={setOpenPopper} title={i18next.t('entityPage.activityLog.header')} side="left">
-                <TabContext value={tab}>
-                    <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-                        <TabList onChange={(event: React.SyntheticEvent, newValue: string) => {
-                            setTab(newValue);
-                        }} centered>
-                            <Tab label={i18next.t('entityPage.activityLog.actions')} value={i18next.t('entityPage.activityLog.actions')} />
-                            <Tab label={i18next.t('entityPage.activityLog.viewers')} value={i18next.t('entityPage.activityLog.viewers')} />
-                        </TabList>
-                    </Box>
-                    <TabPanel value={i18next.t('entityPage.activityLog.actions')}><InfiniteScroll<IActivityLog>
-                        queryKey={['getActivityLogRequest', entityId]}
-                            queryFunction={({ pageParam }) =>
-                                getActivityLogRequest(entityId, infiniteScrollPageCount, pageParam, [
-                                    'DELETE_RELATIONSHIP',
-                                    'CREATE_RELATIONSHIP',
-                                    'UPDATE_ENTITY',
-                                    'CREATE_ENTITY',
-                                    'DISABLE_ENTITY',
-                                    'ACTIVATE_ENTITY',
-                                ])
-                            }
-                        onQueryError={(error) => {
-                            // eslint-disable-next-line no-console
-                            console.log('failed to get activities. error:', error);
-                            toast.error(i18next.t('entityPage.activityLog.failedToGetActivities'));
-                        }}
-                        getNextPageParam={(lastPage, allPages) => {
-                            const nextPage = allPages.length * infiniteScrollPageCount;
-                            return lastPage.length ? nextPage : undefined;
-                        }}
-                        endText={i18next.t('entityPage.activityLog.noSearchLeft')}
-                    >
-                        {(activityLog) => (
-                            <>
-                                <ActivityLogRow log={activityLog} entityTemplate={entityTemplate} />
-                                <Divider variant="middle" style={{ marginTop: '7px' }} />
-                            </>
-                        )}
-                    </InfiniteScroll></TabPanel>
-                    <TabPanel value={i18next.t('entityPage.activityLog.viewers')}>
-                        <InfiniteScroll<IActivityLog>
-                        queryKey={['getActivityLogRequest', entityId]}
-                        queryFunction={({ pageParam }) => getActivityLogRequest(entityId, infiniteScrollPageCount, pageParam, ['VIEW_ENTITY'])}
-
-                        onQueryError={(error) => {
-                            // eslint-disable-next-line no-console
-                            console.log('failed to get activities. error:', error);
-                            toast.error(i18next.t('entityPage.activityLog.failedToGetActivities'));
-                        }}
-                        getNextPageParam={(lastPage, allPages) => {
-                            const nextPage = allPages.length * infiniteScrollPageCount;
-                            return lastPage.length ? nextPage : undefined;
-                        }}
-                        endText={i18next.t('entityPage.activityLog.noSearchLeft')}
-                    >
-                        {(activityLog) => (
-                            <>
-                                <ActivityLogRow log={activityLog} entityTemplate={entityTemplate} />
-                                <Divider variant="middle" style={{ marginTop: '7px' }} />
-                            </>
-                        )}
-                    </InfiniteScroll>
-                    </TabPanel>
-                </TabContext>
-
+                <InfiniteScroll<IActivityLog>
+                    queryKey={['getActivityLogRequest', entityId]}
+                    queryFunction={({ pageParam }) =>
+                        getActivityLogRequest(entityId, infiniteScrollPageCount, pageParam, [
+                            'DELETE_RELATIONSHIP',
+                            'CREATE_RELATIONSHIP',
+                            'UPDATE_ENTITY',
+                            'CREATE_ENTITY',
+                            'DISABLE_ENTITY',
+                            'ACTIVATE_ENTITY',
+                        ])
+                    }
+                    onQueryError={(error) => {
+                        // eslint-disable-next-line no-console
+                        console.log('failed to get activities. error:', error);
+                        toast.error(i18next.t('entityPage.activityLog.failedToGetActivities'));
+                    }}
+                    getNextPageParam={(lastPage, allPages) => {
+                        const nextPage = allPages.length * infiniteScrollPageCount;
+                        return lastPage.length ? nextPage : undefined;
+                    }}
+                    endText={i18next.t('entityPage.activityLog.noSearchLeft')}
+                >
+                    {(activityLog) => (
+                        <>
+                            <ActivityLogRow log={activityLog} entityTemplate={entityTemplate} />
+                            <Divider variant="middle" style={{ marginTop: '7px' }} />
+                        </>
+                    )}
+                </InfiniteScroll>
             </PopperSidebar>
         </>
     );
