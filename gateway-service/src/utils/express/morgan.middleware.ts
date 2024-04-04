bbbcd93699772fda1fe4cf741906ea83a3283933
yonatan-bardin <yonatanbardin5@gmail.com>
2024-04-04 07:53:58 +0300

    feat: add logs to all routes in the system(from the gateway-service

--- gateway-service/src/utils/express/morgan.middleware.ts
+++ gateway-service/src/utils/express/morgan.middleware.ts
@@ -2,13 +2,20 @@ import morgan, { StreamOptions } from 'morgan';
 import logger from '../logger';
 import config from '../../config';
 
-const stream: StreamOptions = { write: (message) => logger.info(message) };
-morgan.token('logging-id', (req) => req.headers['logging-id'] as string);
-morgan.format('jsonFormat', (req: any) => {
+const stream: StreamOptions = {
+    write: (message) => {
+        const logObject = JSON.parse(message);
+
+        logger.info('morgan-log', logObject);
+    },
+};
+morgan.format('jsonFormat', (_tokens: any, req: any, _res: any) => {
     const logObject = {
         userId: req?.user?.id,
         serviceName: config.logs.extraDefault.serviceName,
         request: req.path,
+        method: req.method,
+        data: req.body,
     };
 
     return JSON.stringify(logObject);
