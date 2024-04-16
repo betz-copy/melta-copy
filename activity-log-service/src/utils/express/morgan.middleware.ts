7b61c5e316aaaeb2b45905705abe5774afa59479
Yonatan bardin <yonatanbardin5@gmail.com>
2024-04-16 10:33:29 +0300

    del: remove unused middlewares

--- activity-log-service/src/utils/express/morgan.middleware.ts
+++
@@ -1,7 +0,0 @@
-import * as morgan from 'morgan';
-import logger from '../logger/logsLogger';
-
-const stream: morgan.StreamOptions = { write: (message) => logger.info(message) };
-const morganMiddleware = morgan('combined', { stream });
-
-export default morganMiddleware;
--- global-search-service/src/utils/express/morgan.middleware.ts
+++
@@ -1,7 +0,0 @@
-import morgan, { StreamOptions } from 'morgan';
-import logger from '../logger/logsLogger';
-
-const stream: StreamOptions = { write: (message) => logger.info(message) };
-const morganMiddleware = morgan('combined', { stream });
-
-export default morganMiddleware;
