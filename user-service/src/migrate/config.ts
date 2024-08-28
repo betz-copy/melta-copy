30bdda1a86587fc4bab688d1bcf954cd022dc81e
Nadav Hammer <twindata36@gmail.com>
2024-08-28 18:04:27 +0300

    feat: users migration script

---
+++ user-service/src/migrate/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    workspaceId: '66cdac120d2dd5e6b853f694',
+    oldMeltaUrl: 'https://melta',
+    newMeltaUrl: 'https://new-melta',
+    bearerToken: 'Bearer token',
+} as const;
