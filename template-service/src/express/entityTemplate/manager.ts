971a8035c245f5a617898799bd98758c2fe4a269
Mayan Raz <mayanr2005@gmail.com>
2024-10-29 15:38:21 +0200

    support update relationship

--- template-service/src/express/entityTemplate/manager.ts
+++ template-service/src/express/entityTemplate/manager.ts
@@ -57,13 +57,17 @@ export class EntityTemplateManager extends DefaultManagerMongo<IMongoEntityTempl
         return Object.values(entityTemplate.properties.properties).some((property) => property.relationshipReference);
     }
 
-    async upsertRelationshipsProperties(entityTemplate: IMongoEntityTemplate, session?: ClientSession) {
+    async upsertRelationshipsProperties(entityTemplate: IMongoEntityTemplate, session?: ClientSession, isEditMode?: boolean) {
         const fixedEntityTemplate: IMongoEntityTemplate = JSON.parse(JSON.stringify(entityTemplate));
 
         await Promise.all(
             Object.entries(fixedEntityTemplate.properties.properties).map(async ([propertyName, propertyTemplate]) => {
                 if (propertyTemplate.format === 'relationshipReference' && propertyTemplate.relationshipReference) {
-                    const { relationshipTemplateDirection: relationshipDirection, relatedTemplateId } = propertyTemplate.relationshipReference;
+                    const {
+                        relationshipTemplateDirection: relationshipDirection,
+                        relatedTemplateId,
+                        relationshipTemplateId,
+                    } = propertyTemplate.relationshipReference;
 
                     const relationshipTemplateToUpsert: IRelationshipTemplate = {
                         sourceEntityId: relationshipDirection === 'outgoing' ? fixedEntityTemplate._id : relatedTemplateId,
@@ -73,11 +77,18 @@ export class EntityTemplateManager extends DefaultManagerMongo<IMongoEntityTempl
                         isProperty: true,
                     };
 
-                    const upsertedRelationshipTemplate = await this.relationshipTemplateManager.createTemplate(relationshipTemplateToUpsert, session);
+                    if (isEditMode && relationshipTemplateId) {
+                        await this.relationshipTemplateManager.updateTemplateById(relationshipTemplateId, relationshipTemplateToUpsert, session);
+                    } else {
+                        const upsertedRelationshipTemplate = await this.relationshipTemplateManager.createTemplate(
+                            relationshipTemplateToUpsert,
+                            session,
+                        );
 
-                    // eslint-disable-next-line no-param-reassign
-                    fixedEntityTemplate.properties.properties[propertyName].relationshipReference!.relationshipTemplateId =
-                        upsertedRelationshipTemplate._id.toString();
+                        // eslint-disable-next-line no-param-reassign
+                        fixedEntityTemplate.properties.properties[propertyName].relationshipReference!.relationshipTemplateId =
+                            upsertedRelationshipTemplate._id.toString();
+                    }
                 }
             }),
         );
@@ -169,7 +180,7 @@ export class EntityTemplateManager extends DefaultManagerMongo<IMongoEntityTempl
             let entityTemplateToUpdate = { ...currentEntityTemplate, ...updatedTemplateData };
 
             if (this.hasRelationshipsProperties(entityTemplateToUpdate)) {
-                entityTemplateToUpdate = await this.upsertRelationshipsProperties(entityTemplateToUpdate, session);
+                entityTemplateToUpdate = await this.upsertRelationshipsProperties(entityTemplateToUpdate, session, true);
             }
 
             const updatedEntityTemplate = await this.model
