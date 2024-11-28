d822dd607f3dee62b83ddbdfaef2503c9b968fb5
Mayan Raz <mayanr2005@gmail.com>
2024-11-28 17:07:00 +0200

    fix: excel row limit

--- gateway-service/src/express/instances/manager.ts
+++ gateway-service/src/express/instances/manager.ts
@@ -180,6 +180,7 @@ export class InstancesManager extends DefaultManagerProxy<InstancesService> {
                 template,
                 displayColumns,
                 workspace,
+                skip,
             );
         }
     }
