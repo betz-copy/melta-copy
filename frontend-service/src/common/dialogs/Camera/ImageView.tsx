a7c1911ac54857e42b5ef21f7dac75c65a4de8e1
Idan <idan.malbin@gmail.com>
2024-12-05 17:47:46 +0200

    feat (ImageView): picture pdf file with crop options

--- frontend-service/src/common/dialogs/Camera/ImageView.tsx
+++ frontend-service/src/common/dialogs/Camera/ImageView.tsx
@@ -1,9 +1,15 @@
-import { Grid, IconButton, TextField } from '@mui/material';
-import React, { useState } from 'react';
-import { Check as CheckIcon, Close as CloseIcon } from '@mui/icons-material';
+import { Box, Grid, IconButton, TextField } from '@mui/material';
+import React, { useRef, useState } from 'react';
+import { Check as CheckIcon, Close as CloseIcon, PictureAsPdf } from '@mui/icons-material';
 import i18next from 'i18next';
 import { Form, Formik, FormikProps } from 'formik';
 import { toast } from 'react-toastify';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { Cropper } from 'react-cropper';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import 'cropperjs/dist/cropper.css';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import jsPDF from 'jspdf';
 
 interface IImageView {
     setStream: React.Dispatch<React.SetStateAction<MediaStream | null>>;
@@ -15,6 +21,7 @@ interface IImageView {
 }
 
 const ImageView: React.FC<IImageView> = ({ setStream, imgURL, setImgURL, setOpen, onPictureTaken, cameraSize }) => {
+    const [usePdf, setUsePdf] = useState(false);
     const [imgName, setImgName] = useState<string | null>(null);
     const iconSize = '23px';
     const { width, height } = cameraSize;
@@ -25,7 +32,7 @@ const ImageView: React.FC<IImageView> = ({ setStream, imgURL, setImgURL, setOpen
         return new File([blob], `${imgName!}.png`);
     };
 
-    const uploadImg = async () => {
+    const handleUploadPng = async () => {
         const file = await urlToFile();
         onPictureTaken(file);
         setOpen(false);
@@ -44,17 +51,67 @@ const ImageView: React.FC<IImageView> = ({ setStream, imgURL, setImgURL, setOpen
         }
     };
 
+    const cropperRef = useRef<any>(null); // Reference to the cropper instance
+
+    const handleExportToPdf = () => {
+        if (!cropperRef.current) return;
+
+        const { cropper } = cropperRef.current;
+        const croppedCanvas = cropper.getCroppedCanvas();
+
+        const croppedImageURL = croppedCanvas.toDataURL('image/png');
+
+        // eslint-disable-next-line new-cap
+        const pdf = new jsPDF();
+        pdf.addImage(croppedImageURL, 'PNG', 10, 10, 190, 0);
+
+        const pdfBlob = pdf.output('blob');
+
+        const file = new File([pdfBlob], `${imgName}.pdf`, { type: 'application/pdf' });
+
+        onPictureTaken(file);
+        setOpen(false);
+        setImgURL(null);
+        setImgName(null);
+    };
+
+    const uploadImgOrPdf = async () => {
+        if (usePdf) handleExportToPdf();
+        else handleUploadPng();
+    };
+
     return (
         <>
-            <Grid style={{ display: 'flex', justifyContent: 'flex-end' }}>
+            <Grid style={{ position: 'absolute', left: '0.2rem', top: '0.2rem' }}>
                 <IconButton onClick={onClose}>
                     <CloseIcon style={{ width: iconSize, height: iconSize }} />
                 </IconButton>
             </Grid>
-            <img src={imgURL} alt="cameraPic" style={{ paddingLeft: '10px', paddingRight: '10px', width, height }} />
+            <Box sx={{ padding: '10px' }}>
+                {usePdf ? (
+                    <Cropper
+                        src={imgURL}
+                        ref={cropperRef}
+                        style={{ width, height }}
+                        zoomTo={0.5}
+                        initialAspectRatio={1}
+                        preview=".img-preview"
+                        viewMode={1}
+                        minCropBoxHeight={10}
+                        minCropBoxWidth={10}
+                        background={false}
+                        responsive
+                        autoCropArea={1}
+                        checkOrientation={false}
+                        guides
+                    />
+                ) : (
+                    <img src={imgURL} alt="cameraPic" style={{ width, height }} />
+                )}
+            </Box>
             <Formik initialValues={{ name: imgName }} onSubmit={async (value) => setImgName(value.name)}>
                 {(formikProps: FormikProps<string>) => (
-                    <Form style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Form style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <TextField
                             label={i18next.t('camera.imgName')}
                             value={imgName}
@@ -63,8 +120,11 @@ const ImageView: React.FC<IImageView> = ({ setStream, imgURL, setImgURL, setOpen
                             type="text"
                             sx={{ width: 260 }}
                         />
-                        <IconButton disabled={!formikProps.values || !imgName} type="submit" onClick={() => uploadImg()}>
+                        <IconButton disabled={!formikProps.values || !imgName} type="submit" onClick={() => uploadImgOrPdf()}>
                             <CheckIcon style={{ color: !formikProps.values || !imgName ? '#CCCFE5' : '#1E2775', width: '25px', height: '25px' }} />
+                        </IconButton>
+                        <IconButton onClick={() => setUsePdf(!usePdf)}>
+                            <PictureAsPdf color={usePdf ? 'primary' : 'disabled'} />
                         </IconButton>
                     </Form>
                 )}
