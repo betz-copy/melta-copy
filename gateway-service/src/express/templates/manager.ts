3dc563a41a7565bcef817883eb5657fa436b8c04
Shirel <shirel123la@gmail.com>
2024-12-08 12:43:04 +0200

    wip

--- gateway-service/src/express/templates/manager.ts
+++ gateway-service/src/express/templates/manager.ts
@@ -457,7 +457,7 @@ export class TemplatesManager extends DefaultManagerProxy<EntityTemplateService>
             throw new BadRequestError('entity template still has instances', { errorCode: entityTemplateHasInstances });
         }
     }
-    const updatedRelationship =
+
     async deleteEntityTemplate(id: string): Promise<IMongoEntityTemplateWithConstraints> {
         const entityTemplateToDelete = await this.entityTemplateService.getEntityTemplateById(id);
         await this.throwIfEntityHasRelationships(entityTemplateToDelete);
