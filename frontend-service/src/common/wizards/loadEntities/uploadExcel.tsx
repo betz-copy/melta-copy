54e851787f54ce00042a022d63a1295dfcff55f2
Mayan Raz <mayanr2005@gmail.com>
2024-12-11 10:25:35 +0200

    refactor: move read excel to hook

--- frontend-service/src/common/wizards/loadEntities/uploadExcel.tsx
+++ frontend-service/src/common/wizards/loadEntities/uploadExcel.tsx
@@ -1,20 +1,19 @@
 import React, { useState } from 'react';
 import { FormikProps } from 'formik';
 import i18next from 'i18next';
-import * as XLSX from 'xlsx';
 import { Grid, Typography, useTheme } from '@mui/material';
 import { v4 as uuid } from 'uuid';
-import { toast } from 'react-toastify';
 import { EntitiesWizardValues, ISteps, StepStatus } from '.';
 import { IMongoEntityTemplatePopulated } from '../../../interfaces/entityTemplates';
 import OpenPreview from '../../FilePreview/OpenPreview';
 import { environment } from '../../../globals';
 import EntitiesTableOfTemplate from '../../EntitiesTableOfTemplate';
 import { InstanceFileInput } from '../../inputs/InstanceFilesInput/InstanceFileInput';
+import { useReadExcel } from '../../../utils/hooks/useReadExcel';
 
 const {
     agGrid: { defaultRowHeight, defaultFontSize },
-    loadExcel: { excelExtension, acceptedFilesTypes, filesLimit, entitiesLimit },
+    loadExcel: { excelExtension, acceptedFilesTypes },
 } = environment;
 
 export const UploadExcel: React.FC<{
@@ -27,85 +26,9 @@ export const UploadExcel: React.FC<{
     const theme = useTheme();
     const { values, setFieldValue, setFieldTouched } = formikProps;
 
-    const [rowData, setRowData] = useState<any[]>([]);
     const [errorText, setErrorText] = useState<string | undefined>();
 
-    const convertFileDataToRowData = (gridData: any[][], headers: string[]) => {
-        return gridData
-            .map((row) => {
-                const rowObject: { [key: string]: any } = {};
-                headers.forEach((header, index) => {
-                    const key = Object.entries(template.properties.properties).find(([_, value]) => value.title === header)![0];
-                    rowObject[key] = row[index] || '';
-                });
-
-                const isEmptyRow = Object.values(rowObject).every((value) => value === '' || value === undefined);
-
-                return isEmptyRow ? null : { properties: rowObject };
-            })
-            .filter((rowObject) => rowObject !== null);
-    };
-
-    const importDataToGrid = (fileData: any[][]): { properties: Record<string, any> }[] | null => {
-        if (fileData.length === 0) return null;
-
-        const headers = fileData[0] as string[];
-        const newRows = convertFileDataToRowData(fileData.slice(1), headers);
-        return newRows || null;
-    };
-
-    const handleFilesChange = async (files: File[]): Promise<boolean> => {
-        if (!files || files.length === 0) return false;
-
-        setRowData([]);
-
-        const entities: { properties: Record<string, any> }[] = [];
-        const fileReadPromises = Array.from(files).map((file) => {
-            return new Promise<void>((resolve, reject) => {
-                const reader = new FileReader();
-                reader.onload = (e) => {
-                    try {
-                        const binaryData = e.target?.result as ArrayBuffer;
-                        const workbook = XLSX.read(binaryData, { type: 'array' });
-                        const firstSheetName = workbook.SheetNames[0];
-                        const worksheet = workbook.Sheets[firstSheetName];
-
-                        const worksheetName = workbook?.Workbook?.Sheets?.[0]?.name as string;
-                        if (!template.displayName.includes(worksheetName)) throw new Error('Invalid File: wrong template');
-                        const fileData: any[][] = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-
-                        const newEntities = importDataToGrid(fileData);
-                        if ((newEntities?.length || 0) > entitiesLimit) {
-                            reject(new Error(file.name));
-                        } else {
-                            newEntities?.forEach((newEntity) => entities.push(newEntity));
-                            resolve();
-                        }
-                    } catch (err) {
-                        reject(err);
-                    }
-                };
-                reader.onerror = () => reject(new Error('Error reading file'));
-                reader.readAsArrayBuffer(file);
-            });
-        });
-
-        try {
-            await Promise.all(fileReadPromises);
-            if (entities.length === 0) {
-                toast.warn(i18next.t('wizard.entity.loadEntities.emptyExcel'));
-                return false;
-            }
-            setRowData(entities);
-            return true;
-        } catch (error) {
-            if (files.some((file) => file.name === (error as Error).message))
-                toast.error(`${i18next.t('wizard.entity.loadEntities.limitNumberEntities')} ${(error as Error).message}`);
-            else if ((error as Error).message.includes('wrong template')) toast.error(i18next.t('wizard.entity.loadEntities.filesWrongTemplate'));
-            else toast.error(i18next.t('wizard.entity.loadEntities.failedReadingFiles'));
-            return false;
-        }
-    };
+    const { readFile, rowData } = useReadExcel();
 
     if (stepsData.status === StepStatus.uploadExcel)
         return (
@@ -122,18 +45,7 @@ export const UploadExcel: React.FC<{
                 error={errorText || formikProps.errors.files}
                 setErrorText={setErrorText}
                 onDrop={async (files) => {
-                    const validFiles = files.filter((file): file is File => file !== null);
-
-                    if (validFiles.length > filesLimit) {
-                        toast.error('wizard.entity.loadEntities.limitNumberFiles');
-                        return;
-                    }
-                    const filesObject = validFiles.reduce<Record<string, File>>((acc, file) => {
-                        return { ...acc, [file.name]: file };
-                    }, {});
-
-                    const isSucceed = await handleFilesChange(validFiles);
-                    if (isSucceed) setStepsData((prev) => ({ ...prev, status: StepStatus.previewExcelRows, files: filesObject }));
+                    await readFile(files as File[], template, setStepsData);
                 }}
             />
         );
