4b540c5460703125dd1ad841762ef4037f607d37
AmitE25 <etrogyam@gmail.com>
2024-12-18 14:37:44 +0200

    pull form dev

---
+++ gateway-service/src/express/iFrames/controller.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { IFrameManager } from './manager';
+import DefaultController from '../../utils/express/controller';
+import { RequestWithPermissionsOfUserId } from '../../utils/authorizer';
+
+export class IFrameController extends DefaultController<IFrameManager> {
+    constructor(workspaceId: string) {
+        super(new IFrameManager(workspaceId));
+    }
+
+    async searchIFrames(req: Request, res: Response) {
+        const { body, permissionsOfUserId } = req as RequestWithPermissionsOfUserId;
+
+        res.json(await this.manager.searchIFrames(body, permissionsOfUserId));
+    }
+
+    async getIFrameById(req: Request, res: Response) {
+        const {
+            params: { iFrameId },
+        } = req as RequestWithPermissionsOfUserId;
+
+        res.json(await this.manager.getIFrameById(iFrameId));
+    }
+
+    async createIFrame(req: Request, res: Response) {
+        res.json(await this.manager.createIFrame(req.body, req.file));
+    }
+
+    async deleteIFrame(req: Request, res: Response) {
+        res.json(await this.manager.deleteIFrame(req.params.iFrameId));
+    }
+
+    async updateIFrame(req: Request, res: Response) {
+        res.json(await this.manager.updateIFrame(req.params.iFrameId, req.body, req.file));
+    }
+}
+
+export default IFrameController;
---
+++ scripts/migrations/semantic-migrate-script/src/services/workspace.ts
@@ -0,0 +1,72 @@
+import axios from 'axios';
+import config from '../config';
+
+const {
+    services: { workspacesUri, baseRoute },
+} = config;
+
+export enum Colors {
+    primary = 'primary',
+}
+
+export enum WorkspaceTypes {
+    dir = '',
+    mlt = '.mlt',
+}
+
+export interface IMetadata {
+    shouldNavigateToEntityPage: boolean;
+    isDrawerOpen: boolean;
+    agGrid: {
+        rowCount: number;
+        defaultExpandedRowCount: number;
+        defaultRowHeight: number;
+        defaultFontSize: number;
+        cacheBlockSize: number;
+        maxConcurrentDatasourceRequests: number;
+        infiniteInitialRowCount: number;
+        defaultExpandedTableHeight: number;
+        paginationPageSizeSelector: Array<number>;
+    };
+    activityLog: {
+        infiniteScrollPageCount: number;
+    };
+    processInstances: {
+        infiniteScrollPageCount: number;
+    };
+    permission: {
+        infiniteScrollPageCount: number;
+    };
+    mainFontSizes: {
+        headlineTitleFontSize: string;
+        headlineSubTitleFontSize: string;
+    };
+    smallPreviewHeight: {
+        number: string;
+        unit: string;
+    };
+    iconSize: {
+        width: string;
+        height: string;
+    };
+}
+export interface IWorkspace {
+    _id: string;
+    name: string;
+    displayName: string;
+    path: string;
+    type: WorkspaceTypes;
+    colors: Record<Colors, string>;
+    iconFileId?: string;
+    logoFileId?: string;
+    metadata?: Partial<IMetadata>;
+}
+
+export class WorkspaceService {
+    private static workspaceService = axios.create({ baseURL: `${workspacesUri}${baseRoute}` });
+
+    static async getWorkspaceIds(type: IWorkspace['type']) {
+        const { data } = await this.workspaceService.post<string[]>(`/ids`, { type });
+        return data;
+    }
+}
