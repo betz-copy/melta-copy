743d313e5516294ec0c0f150e6c3c159f6cd6200
Mayan Raz <mayanr2005@gmail.com>
2024-12-18 13:19:06 +0200

    finished

--- frontend-service/src/utils/HighlightText.tsx
+++ frontend-service/src/utils/HighlightText.tsx
@@ -1,15 +1,16 @@
 import { escapeRegExp } from 'lodash';
 import React from 'react';
+import { VerifyLink } from '../common/VerifyLink';
 
 export const HighlightText: React.FC<{ text?: string | React.JSX.Element; searchedText?: string }> = ({ text, searchedText }) => {
     // eslint-disable-next-line react/jsx-no-useless-fragment
-    if ((typeof text !== 'string' && typeof text !== 'number') || !searchedText) return <>{text}</>;
+    if ((typeof text !== 'string' && typeof text !== 'number') || !searchedText) return <VerifyLink>{text}</VerifyLink>;
 
     return (
-        <span>
+        <VerifyLink>
             {String(text)
                 .split(new RegExp(`(${escapeRegExp(searchedText)})`, 'gi'))
                 .map((part) => (part.toLowerCase() === searchedText.toLowerCase() ? <b key={part}>{part}</b> : part))}
-        </span>
+        </VerifyLink>
     );
 };
