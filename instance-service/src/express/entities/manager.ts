fcf3116792dffebaa265be076260ac4a22b4b14d
Itay Tovim <optimusp024@gmail.com>
2024-12-23 16:23:38 +0200

    fix: hata folder upsert

--- instance-service/src/express/entities/manager.ts
+++ instance-service/src/express/entities/manager.ts
@@ -1381,14 +1381,14 @@ export class EntityManager extends DefaultManagerNeo4j {
 
     async updateRelationshipReferencesEnumField(
         templateId: string,
-        ogirinalEntities: IEntity[],
+        originalEntities: IEntity[],
         newValue: string,
         oldValue: string,
         field: any,
         transaction: Transaction,
     ) {
         let updateRelatedEntitiesQuery;
-        const originalChangedEntityIds = ogirinalEntities.map((node) => node.properties._id);
+        const originalChangedEntityIds = originalEntities.map((node) => node.properties._id);
         const entitiesNeedToUpdate = await this.getRelatedEntitiesOfEntity(templateId, originalChangedEntityIds, transaction);
 
         await Promise.all(
