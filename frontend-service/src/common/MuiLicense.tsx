ba254943c4d190aa18c25f94c58c3b5b631f931a
Itay Tovim <optimusp024@gmail.com>
2025-01-08 12:04:12 +0200

    fix: config mui key

--- frontend-service/src/common/MuiLicense.tsx
+++ frontend-service/src/common/MuiLicense.tsx
@@ -1,6 +1,11 @@
 import { LicenseInfo } from '@mui/x-license-pro';
+import { environment } from '../globals';
 
-LicenseInfo.setLicenseKey('8c5d3fd6a4390125cf8590dea55dbb05Tz1HdXksRT0yMDAwMDAwMDAwMDAwLFM9cHJlbWl1bSxMTT1zdWJzY3JpcHRpb24sS1Y9Mg==');
+const {
+    mui: { activationKey },
+} = environment;
+
+LicenseInfo.setLicenseKey(activationKey);
 
 const MuiXLicense = () => {
     return null;
