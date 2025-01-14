3a41bef3d0f4b7c2ee5e35ce048dbb402676ba14
Nadav Beladev <nadybeladev@gmail.com>
2025-01-14 10:59:43 +0200

    fix: dark mode enum color

--- frontend-service/src/common/ColoredEnumChip.tsx
+++ frontend-service/src/common/ColoredEnumChip.tsx
@@ -1,4 +1,5 @@
 import { Chip } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 import React, { CSSProperties } from 'react';
 import { colorWithOpacity } from '../utils/colorUtils';
 
@@ -13,14 +14,23 @@ interface ColoredEnumChipProps {
 }
 
 export const ColoredEnumChip: React.FC<ColoredEnumChipProps> = ({ label, color, style, icon, searchValue, onDelete, deleteIcon }) => {
+    const theme = useTheme();
+    const isDarkMode = theme.palette.mode === 'dark';
+
     const shouldHighlight = Boolean(searchValue && label.toString().includes(searchValue));
-    const textColor = color === 'default' ? '#000' : color;
+    // eslint-disable-next-line no-nested-ternary
+    const textColor = color === 'default' ? (isDarkMode ? '#fff' : '#000') : color;
 
     let backgroundColor: string;
     if (color !== 'default' && color) {
         backgroundColor = colorWithOpacity(color, shouldHighlight ? 0.25 : 0.1);
     } else {
-        backgroundColor = shouldHighlight ? '#d3d1d1' : '#F7F7F7';
+        // eslint-disable-next-line no-lonely-if
+        if (isDarkMode) {
+            backgroundColor = shouldHighlight ? '#4F4F4F' : '#303030';
+        } else {
+            backgroundColor = shouldHighlight ? '#d3d1d1' : '#F7F7F7';
+        }
     }
 
     return (
@@ -34,7 +44,7 @@ export const ColoredEnumChip: React.FC<ColoredEnumChipProps> = ({ label, color, 
                 height: '25px',
                 borderRadius: '6px',
                 border: 0,
-                fontWeight: shouldHighlight ? '700' : '400',
+                fontWeight: shouldHighlight ? 700 : 400,
                 color: textColor,
                 backgroundColor,
                 fontFamily: 'Rubik',
