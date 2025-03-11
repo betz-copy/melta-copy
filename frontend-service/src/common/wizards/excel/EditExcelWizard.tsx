79183ef604e0380d55f3800a993a6c35325ae77d
Mayan Raz <mayanr2005@gmail.com>
2025-03-11 15:04:46 +0200

    mini fix

--- frontend-service/src/common/wizards/excel/EditExcelWizard.tsx
+++ frontend-service/src/common/wizards/excel/EditExcelWizard.tsx
@@ -89,7 +89,7 @@ const EditExcelWizard: React.FC<WizardBaseType<EntitiesWizardValues>> = ({
                 setStepsData((prev) => ({ ...prev, status: ExcelStepStatus.excelUploadResult, data }));
                 return data;
             },
-            onError(error) {
+            onError() {
                 toast.error(i18next.t('wizard.entity.loadEntities.failedLoadEntities'));
                 onClose();
             },
