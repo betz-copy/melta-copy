ca7d0339ffc6c23df091f1c851043d4732eb61cf
Yonatan bardin <yonatanbardin5@gmail.com>
2025-04-10 13:26:36 +0300

    jkljk

--- frontend-service/src/pages/Graph/GraphFilterToBackend.tsx
+++ frontend-service/src/pages/Graph/GraphFilterToBackend.tsx
@@ -44,14 +44,14 @@ const filterFieldToValue: Record<keyof IFilterOfField, string> = {
     $eqi: 'equals',
 };
 
-const handleRegexFilter = (filterValue: string, not: boolean = false): IAGGridTextFilter | null => {
+const handleRegexFilter = (filterValue: string): IAGGridTextFilter | null => {
     const startsWith = filterValue.startsWith('.*');
     const endsWith = filterValue.endsWith('.*');
 
     if (startsWith && endsWith)
         return {
             filterType: 'text',
-            type: not ? 'notContains' : 'contains',
+            type: 'contains',
             filter: filterValue.slice(2, -2),
         } as IAGGridTextFilter;
 
@@ -107,16 +107,6 @@ const translateFieldFilter = (fieldFilter: IFilterOfField, { type, format }: IEn
             if (filterKey === '$rgx' && typeof filterValue === 'string') {
                 const regexFilter = handleRegexFilter(filterValue);
                 if (regexFilter) return regexFilter;
-            }
-            if (filterKey === '$not' && typeof filterValue === 'object') {
-                const notFilter = filterValue as IFilterOfField;
-                if ('$rgx' in notFilter) {
-                    return {
-                        filterType: 'text',
-                        type: 'notContains',
-                        filter: handleRegexFilter(notFilter.$rgx as string)!.filter,
-                    } as IAGGridTextFilter;
-                }
             }
 
             return {
