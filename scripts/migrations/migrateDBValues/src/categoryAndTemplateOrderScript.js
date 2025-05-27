04bfb238f2044c1429c5364eeadd62b5e3be85d3
Uri Handelzalts <urihandelzalts@gmail.com>
2025-05-27 10:56:43 +0300

    feat(several): wip - migration script.

--- scripts/migrations/migrateDBValues/src/categoryAndTemplateOrderScript.js
+++ scripts/migrations/migrateDBValues/src/categoryAndTemplateOrderScript.js
@@ -1,99 +1,178 @@
-import mongoose from "mongoose";
-import config from "./config/index.js";
+import mongoose from 'mongoose';
+import config from './config/index.js';
+import { ObjectId } from 'mongodb';
 
 const { mongo } = config;
 
 // AGGREGATIONS
 
 export const getCategoryIds = [
-  {
-    $sort: {
-      createdAt: 1,
+    {
+        $sort: {
+            createdAt: 1,
+        },
     },
-  },
-  {
-    $project: {
-      _id: 0,
-      id: { $toString: "$_id" },
+    {
+        $project: {
+            _id: 0,
+            id: { $toString: '$_id' },
+        },
     },
-  },
 ];
 
-export const getTemplateOrderPipeline = (categoryId) => {
-  return [
-    {
-      $match: {
-        category: categoryId,
-      },
-    },
-    {
-      $project: {
-        _id: 0,
-        id: { $toString: "$_id" },
-      },
-    },
-  ];
+export const getTemplateIds = (categoryId) => {
+    return [
+        {
+            $match: {
+                category: new ObjectId(categoryId),
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                id: { $toString: '$_id' },
+            },
+        },
+    ];
 };
+
+// export const setTemplateOrder = (categoryId, templateIds) => {
+//     return [
+//         {
+//             $match: {
+//                 _id: categoryId,
+//             },
+//         },
+//         {
+//             $set: {
+//                 templatesOrder: templateIds,
+//             },
+//         },
+//     ];
+// };
 
 // MIGRATION SCRIPT
 
 const connectToMongo = async () => {
-  await mongoose.connect(mongo.uri);
+    await mongoose.connect(mongo.uri);
 
-  console.log("Connected to MongoDB");
+    console.log('Connected to MongoDB');
 };
 
 const listDatabasesWithMongoose = async () => {
-  const adminDb = mongoose.connection.db.admin();
-  const result = await adminDb.listDatabases();
-  console.log("Databases:", result.databases);
+    const adminDb = mongoose.connection.db.admin();
+    const result = await adminDb.listDatabases();
+    console.log('Databases:', result.databases);
 
-  return result.databases;
+    return result.databases;
 };
 
 const getDBCategoryIds = async (db) => {
-  //   const db = mongoose.connection.client.db(dbName);
-  const collections = await db.listCollections().toArray();
-  const collectionNames = collections.map((col) => col.name);
+    //   const db = mongoose.connection.client.db(dbName);
+    const collections = await db.listCollections().toArray();
+    const collectionNames = collections.map((col) => col.name);
 
-  if (collectionNames.includes(mongo.categoriesCollection)) {
-    console.log(
-      `Applying aggregation to ${db.databaseName}.${mongo.categoriesCollection}`
+    if (collectionNames.includes(mongo.categoriesCollection)) {
+        console.log(`Applying aggregation to ${db.databaseName}.${mongo.categoriesCollection}`);
+
+        const pipeline = getCategoryIds;
+        const results = await db.collection(mongo.categoriesCollection).aggregate(pipeline).toArray();
+
+        return results.map((document) => document.id);
+    }
+
+    return null;
+};
+
+const getDBTemplateIds = async (db, categoryId) => {
+    const collections = await db.listCollections().toArray();
+    const collectionNames = collections.map((col) => col.name);
+
+    if (collectionNames.includes(mongo.templatesCollection)) {
+        console.log(`Applying aggregation to ${db.databaseName}.${mongo.templatesCollection}`);
+
+        const pipeline = getTemplateIds(categoryId);
+        const results = await db.collection(mongo.templatesCollection).aggregate(pipeline).toArray();
+
+        return results;
+    }
+
+    return null;
+};
+
+const createCategoryOrder = (db, categoryIds) => {
+    try {
+        return db.collection(mongo.configsCollection).insertOne({
+            type: 'categoryOrder',
+            order: categoryIds,
+        });
+    } catch (error) {
+        console.log(`Failed to create categoryOrder at workspace ${db.databaseName}`);
+    }
+};
+
+const setTemplateOrderDB = async (dbName, categoryId, templateIds) => {
+    const db = mongoose.connection.client.db(dbName);
+    const collections = await db.listCollections().toArray();
+    const collectionNames = collections.map((col) => col.name);
+
+    if (collectionNames.includes(mongo.categoriesCollection)) {
+        console.log(`Applying setTemplateOrder aggregation to category ${categoryId} at ${db.databaseName}.${mongo.categoriesCollection}`);
+
+        // const pipeline = setTemplateOrder(categoryId, templateIds);
+        try {
+            // await db.collection(mongo.categoriesCollection).aggregate(pipeline);
+            await db.collection(mongo.categoriesCollection).updateOne({ _id: new ObjectId(categoryId) }, { $set: { templatesOrder: templateIds } });
+        } catch (error) {
+            console.error(`Failed to add templateOrder to category ${categoryId}, error: ${error}`);
+        }
+    }
+};
+
+const setCategoryAndTemplateOrder = async (dbList) => {
+    await Promise.all(
+        dbList.map(async (database) => {
+            const db = mongoose.connection.client.db(database.name);
+            const categoryIds = await getDBCategoryIds(db);
+
+            if (categoryIds) {
+                try {
+                    await db.collection(mongo.configsCollection).insertOne({
+                        type: 'categoryOrder',
+                        order: categoryIds,
+                    });
+                } catch (error) {
+                    console.log(`Failed to create categoryOrder at workspace ${db.databaseName}`);
+                }
+
+                await Promise.all(
+                    categoryIds.map(async (id) => {
+                        const templateIds = await getDBTemplateIds(db, id);
+
+                        return setTemplateOrderDB(db.databaseName, id, templateIds);
+                    }),
+                );
+            }
+        }),
     );
 
-    const pipeline = getCategoryIds;
-    const results = await db
-      .collection(mongo.categoriesCollection)
-      .aggregate(pipeline)
-      .toArray();
-
-    return results;
-  }
-
-  return null;
+    // console.log('Created category orders:', categoryOrder);
 };
 
-const setCategoryOrders = async (dbList) => {
-  const data = [];
+const main = async () => {
+    try {
+        await connectToMongo();
 
-  await Promise.all(
-    dbList.map(async (database) => {
-      const db = mongoose.connection.client.db(database.name);
-      const categoryIds = await getDBCategoryIds(db);
+        const dbList = await listDatabasesWithMongoose();
 
-      if (categoryIds) {
-        const result = await db.collection(mongo.configsCollection).insertOne({
-          name: "categoryOrder",
-          type: "order",
-          order: categoryIds,
-        });
-
-        data.push({ dbName: database.name, categoryOrder: result });
-      }
-    })
-  );
-
-  console.log("Created category orders:", data);
-
-  return data;
+        await setCategoryAndTemplateOrder(dbList);
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        mongoose.connections.forEach((conn) => conn.close());
+    }
 };
+
+main().then(() => {
+    console.log('unique constraints all serial numbers in neo and mongo');
+});
--- scripts/migrations/migrateDBValues/src/config/index.js
+++ scripts/migrations/migrateDBValues/src/config/index.js
@@ -1,21 +1,18 @@
-import env from "env-var";
-import "./dotenv.js";
+import env from 'env-var';
+import './dotenv.js';
 
 const config = {
-  mongo: {
-    uri: env.get("MONGO_URI").required().asString(),
-    templatesCollection: env.get("TEMPLATES_COLLECTION").required().asString(),
-    categoriesCollection: env
-      .get("CATEGORIES_COLLECTION")
-      .required()
-      .asString(),
-    configsCollection: env.get("CONFIGS_COLLECTION").required().asString(),
-  },
-  neo: {
-    uri: env.get("NEO_URI").required().asString(),
-    user: env.get("NEO_USER").required().asString(),
-    password: env.get("PASSWORD").required().asString(),
-  },
+    mongo: {
+        uri: env.get('MONGO_URI').required().asString(),
+        templatesCollection: env.get('TEMPLATES_COLLECTION').required().asString(),
+        categoriesCollection: env.get('CATEGORIES_COLLECTION').required().asString(),
+        configsCollection: env.get('CONFIGS_COLLECTION').required().asString(),
+    },
+    neo: {
+        uri: env.get('NEO_URI').required().asString(),
+        user: env.get('NEO_USER').required().asString(),
+        password: env.get('PASSWORD').required().asString(),
+    },
 };
 
 export default config;
