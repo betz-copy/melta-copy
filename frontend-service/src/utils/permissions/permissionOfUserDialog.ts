520769944ba37e0d94006b4ea5548f7ab61366a7
Nadav Hammer <twindata36@gmail.com>
2025-07-17 13:12:21 +0300

    fix: duplicate templates in permissions

--- frontend-service/src/utils/permissions/permissionOfUserDialog.ts
+++ frontend-service/src/utils/permissions/permissionOfUserDialog.ts
@@ -40,50 +40,40 @@ export type CategoryWithTemplates = IMongoCategory & {
 };
 
 export const createDialogCategories = (
-    entityTemplates: IMongoEntityTemplatePopulated[],
+    entityTemplates: (IMongoEntityTemplatePopulated & { isParentTemplateInDifferentCategory?: boolean })[],
     childTemplates: IMongoChildTemplatePopulated[],
 ): Map<string, CategoryWithTemplates> => {
     const dialogPermissionData: ReturnType<typeof createDialogCategories> = new Map();
 
     for (const child of childTemplates) {
         if (!entityTemplates.find(({ _id, category }) => _id === child.parentTemplate._id && category._id === child.category._id)) {
-            entityTemplates.push({ ...child.parentTemplate, category: child.category });
+            entityTemplates.push({ ...child.parentTemplate, category: child.category, isParentTemplateInDifferentCategory: true });
         }
     }
 
     for (const entityTemplate of entityTemplates) {
         const category: CategoryWithTemplates = {
-            entityTemplates: dialogPermissionData.get(entityTemplate.category._id)?.entityTemplates || [],
             ...entityTemplate.category,
+            entityTemplates: dialogPermissionData.get(entityTemplate.category._id)?.entityTemplates || [],
         };
 
         const displayEntity: entityTemplatePermissionDialog = {
             id: entityTemplate._id,
             name: entityTemplate.displayName,
             childTemplates: [],
+            isParentTemplateInDifferentCategory: entityTemplate.isParentTemplateInDifferentCategory,
         };
 
         for (const child of childTemplates) {
-            if (child.category._id === entityTemplate.category._id) {
-                const childTemplate: childTemplatePermissionDialog = {
+            if (child.category._id === entityTemplate.category._id && child.parentTemplate._id === entityTemplate._id) {
+                displayEntity.childTemplates.push({
                     id: child._id,
                     name: child.displayName,
                     isFilterByCurrentUser: child.isFilterByCurrentUser,
                     isFilterByUserUnit: child.isFilterByUserUnit,
                     viewType: child.viewType,
                     parentTemplateId: child.parentTemplate._id,
-                };
-
-                if (child.parentTemplate._id === entityTemplate._id) {
-                    displayEntity.childTemplates.push(childTemplate);
-                } else if (category.entityTemplates.find(({ id }) => id !== child.parentTemplate._id)) {
-                    category.entityTemplates.push({
-                        id: child.parentTemplate._id,
-                        name: child.parentTemplate.displayName,
-                        childTemplates: [childTemplate],
-                        isParentTemplateInDifferentCategory: true,
-                    });
-                }
+                });
             }
         }
 
