3e12c40a9f2a9e4878d3b32fb21a87bf6a516a28
Shirel <shirel123la@gmail.com>
2025-07-31 11:58:05 +0300

    feat: add search on categories and entities in permission management dialog

--- frontend-service/src/common/PermissionsDialog/RoleDialog.tsx
+++ frontend-service/src/common/PermissionsDialog/RoleDialog.tsx
@@ -3,7 +3,7 @@ import { Form, Formik, FormikProps } from 'formik';
 import i18next from 'i18next';
 import _cloneDeep from 'lodash.clonedeep';
 import _isEqual from 'lodash.isequal';
-import React from 'react';
+import React, { useState } from 'react';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { toast } from 'react-toastify';
 import * as Yup from 'yup';
@@ -31,6 +31,7 @@ const RoleDialog: React.FC<{
     const setUser = useUserStore((state) => state.setUser);
     const workspace = useWorkspaceStore((state) => state.workspace);
     const darkMode = useDarkModeStore((state) => state.darkMode);
+    const [searchText, setSearchText] = useState('');
 
     const defaultEmptyRole = {
         permissions: {},
@@ -170,9 +171,10 @@ const RoleDialog: React.FC<{
                             {/* dont show management permissions to regular user (if dont have at all) */}
                             <ManagePermissions
                                 mode={mode}
-                                dialogPermissionData={createDialogCategories([...entityTemplates.values()], [...childTemplates.values()])}
+                                dialogPermissionData={createDialogCategories([...entityTemplates.values()], [...childTemplates.values()], searchText)}
                                 formikProps={formikProps as FormikProps<PermissionData>}
                                 workspace={workspace}
+                                searchText={{ value: searchText, set: setSearchText }}
                             />
                         </DialogContent>
 
