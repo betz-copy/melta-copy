b830d97944dcb001a82a64e5922ace70057dc5ff
Efrat Layush <efrat71499@gmail.com>
2025-08-31 12:50:30 +0300

    fix : cr fixes

--- frontend-service/src/pages/Map/LocationPreview.tsx
+++ frontend-service/src/pages/Map/LocationPreview.tsx
@@ -1,43 +1,56 @@
-import React, { useEffect, useRef } from 'react';
-import { Cartesian3, Color } from 'cesium';
-import { Viewer, Entity, PolygonGraphics, PointGraphics, PolylineGraphics, BillboardGraphics } from 'resium';
 import * as Cesium from 'cesium';
+import { Cartesian3, Color } from 'cesium';
+import React, { useEffect, useRef } from 'react';
 import { useQueryClient } from 'react-query';
+import { BillboardGraphics, Entity, PointGraphics, PolygonGraphics, PolylineGraphics, Viewer } from 'resium';
+import { IMongoChildTemplatePopulated } from '../../interfaces/childTemplates';
 import { IEntity } from '../../interfaces/entities';
 import { IMongoEntityTemplatePopulated } from '../../interfaces/entityTemplates';
-import { useEntityWithLocationFields } from '../../utils/hooks/useLocation';
-import { locationToWGS84String, jerusalemCoordinates, calculateCenterOfPolygon } from '../../utils/map';
-import { BaseLayers } from './BaseLayers';
 import { BackendConfigState } from '../../services/backendConfigService';
+import { useEntityWithLocationFields } from '../../utils/hooks/useLocation';
+import { calculateCenterOfPolygon, jerusalemCoordinates, locationToWGS84String } from '../../utils/map';
 import { convertWGS94ToECEF } from '../../utils/map/convert';
-import { IMongoChildTemplatePopulated } from '../../interfaces/childTemplates';
+import { BaseLayers } from './BaseLayers';
 
-export const MeltaPolygon = ({ name, polygon, onClick, color }: { name: string; polygon: Cartesian3[]; onClick?: () => void; color?: string }) => {
+const POLYGON_DEFAULT_COLOR = '#11695a';
+const COORDINATE_DEFAULT_COLOR = '#FF006B';
+
+export const MeltaPolygon = ({
+    name,
+    polygon,
+    onClick,
+    color = POLYGON_DEFAULT_COLOR,
+}: {
+    name: string;
+    polygon: Cartesian3[];
+    onClick?: () => void;
+    color?: string;
+}) => {
     const centroid = calculateCenterOfPolygon(polygon);
 
     return (
         <>
             <Entity name={name}>
-                <PolylineGraphics positions={[...polygon, polygon[0]]} material={Color.fromCssColorString('#ffffffff')} width={2} />
-                <PolygonGraphics hierarchy={polygon} material={Color.fromAlpha(Color.fromCssColorString(color ?? '#11695a'), 0.3)} />
+                <PolylineGraphics positions={[...polygon, polygon[0]]} material={Color.WHITE} width={2} />
+                <PolygonGraphics hierarchy={polygon} material={Color.fromAlpha(Color.fromCssColorString(color), 0.3)} />
                 {polygon.map((position, index) => (
                     <Entity key={`${position.x}, ${position.y} - ${index}`} position={position}>
-                        <PointGraphics color={Color.WHITE} outlineColor={Color.fromCssColorString('#ffffffff')} pixelSize={2} outlineWidth={2} />
+                        <PointGraphics color={Color.WHITE} outlineColor={Color.WHITE} pixelSize={2} outlineWidth={2} />
                     </Entity>
                 ))}
             </Entity>
 
             <Entity name={name} description={locationToWGS84String(polygon)} position={centroid} onClick={onClick}>
-                <PointGraphics color={Color.fromCssColorString(color ?? '#11695a')} outlineColor={Color.WHITE} pixelSize={12} outlineWidth={2} />
+                <PointGraphics color={Color.fromCssColorString(color)} outlineColor={Color.WHITE} pixelSize={12} outlineWidth={2} />
             </Entity>
         </>
     );
 };
 
-const getColoredLocationIcon = (hex: string) => {
+const getColoredLocationIcon = (color: string = COORDINATE_DEFAULT_COLOR) => {
     const svg = `
     <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64"
-         viewBox="-12.8 -12.8 89.60 89.60" fill="${hex}" stroke="${hex}">
+         viewBox="-12.8 -12.8 89.60 89.60" fill="${color}" stroke="${color}">
       <path d="M32,0C18.746,0,8,10.746,8,24c0,5.219,1.711,10.008,4.555,13.93
                l16,24C29.414,63.332,30.664,64,32,64s2.586-0.668,3.328-1.781l16-24
                C54.289,34.008,56,29.219,56,24C56,10.746,45.254,0,32,0z
@@ -49,7 +62,7 @@ const getColoredLocationIcon = (hex: string) => {
 
 export const MeltaCoordinate = ({ name, position, onClick, color }: { name: string; position: Cartesian3; onClick?: () => void; color?: string }) => (
     <Entity name={name} description={locationToWGS84String(position)} position={position} onClick={onClick}>
-        <BillboardGraphics image={getColoredLocationIcon(color ?? '#FF006B')} scale={1} verticalOrigin={Cesium.VerticalOrigin.BOTTOM} />
+        <BillboardGraphics image={getColoredLocationIcon(color)} scale={1} verticalOrigin={Cesium.VerticalOrigin.BOTTOM} />
     </Entity>
 );
 
