06ec3bddec940b132c70b4cd26d3717b12c410dc
Itay Tovim <optimusp024@gmail.com>
2025-09-21 23:58:15 +0300

    fix: works with default

--- frontend-service/src/common/UserAvatar/index.tsx
+++ frontend-service/src/common/UserAvatar/index.tsx
@@ -35,8 +35,9 @@ const UserAvatar: React.FC<IUserAvatarProps> = ({
             enabled: !shouldGetKartoffelImage && !userIcon?.isDefaultProfile,
         },
         {
-            queryKey: ['kartoffelImage', user.kartoffelId],
-            queryFn: () => getKartoffelUserProfileRequest(user.kartoffelId!),
+            queryKey: ['kartoffelImage', user.kartoffelId, user._id],
+            // KartoffelId will usually be undefined since the users saved in neo4j will have _id (which is their kartoffelId)
+            queryFn: () => getKartoffelUserProfileRequest(user.kartoffelId ?? user._id!),
             enabled: shouldGetKartoffelImage && !userIcon?.isDefaultProfile,
         },
     ]);
