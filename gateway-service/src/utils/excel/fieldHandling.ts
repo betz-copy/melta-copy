935e1278da668cc49c0e4f9d265e69133e5d465c
Uri Handelzalts <urihandelzalts@gmail.com>
2025-11-19 13:40:04 +0200

    feat(excel-user-loading): finally finish CR :hurtrealbad:

--- gateway-service/src/utils/excel/fieldHandling.ts
+++ gateway-service/src/utils/excel/fieldHandling.ts
@@ -1,15 +1,18 @@
 import {
+    IChildTemplateProperty,
+    IEntitySingleProperty,
     IKartoffelUser,
     IMongoChildTemplatePopulated,
     IMongoEntityTemplatePopulated,
+    IProperties,
     NotFoundError,
     NotFoundErrorTypes,
 } from '@microservices/shared';
 import { UserNotFoundError } from '../../express/error';
 import UsersManager from '../../express/users/manager';
 
-function normalizeUser(identityCard: string, usersMap: Map<string, IKartoffelUser>, key: string, allAttemptedIds: string[]): IKartoffelUser {
-    const user: IKartoffelUser | undefined = usersMap.get(identityCard);
+const normalizeUser = (identityCard: string, usersMap: Map<string, IKartoffelUser>, key: string, allAttemptedIds: string[]): IKartoffelUser => {
+    const user = usersMap.get(identityCard);
     if (!user)
         throw new NotFoundError('User not found', {
             type: NotFoundErrorTypes.userNotFound,
@@ -18,30 +21,36 @@ function normalizeUser(identityCard: string, usersMap: Map<string, IKartoffelUse
         });
 
     return user;
-}
+};
 
-export async function handleUserFields(
-    template: IMongoEntityTemplatePopulated | IMongoChildTemplatePopulated,
+const updateKartoffelFields = (
+    user: IKartoffelUser,
+    relatedFieldKey: string,
+    templateProperties: IProperties['properties'] | Record<string, IEntitySingleProperty & IChildTemplateProperty>,
     entityProperties: Record<string, any>,
-    usersMap: Map<string, IKartoffelUser>,
-): Promise<void> {
-    const templateProperties = template.properties.properties;
-    const kartoffelFields: string[] = Object.entries(templateProperties)
+): void => {
+    const kartoffelFields = Object.entries(templateProperties)
         .filter(([_key, value]) => value?.format === 'kartoffelUserField')
         .map(([key]) => key);
 
+    for (const fieldKey of kartoffelFields) {
+        const templateField = templateProperties[fieldKey];
+        const relatedUserField = templateField.expandedUserField?.relatedUserField;
+        const relatedKartoffelField = templateField.expandedUserField?.kartoffelField;
+
+        if (relatedUserField === relatedFieldKey && relatedKartoffelField)
+            entityProperties[fieldKey] = user[relatedKartoffelField] ?? entityProperties[fieldKey];
+    }
+};
+
+export const handleUserFields = async (
+    template: IMongoEntityTemplatePopulated | IMongoChildTemplatePopulated,
+    entityProperties: Record<string, any>,
+    usersMap: Map<string, IKartoffelUser>,
+): Promise<void> => {
+    const templateProperties = template.properties.properties;
+
     const errors: UserNotFoundError[] = [];
-
-    const updateKartoffelFields = (user: IKartoffelUser, relatedFieldKey: string): void => {
-        for (const fieldKey of kartoffelFields) {
-            const templateField = templateProperties[fieldKey];
-            const relatedUserField: string | undefined = templateField.expandedUserField?.relatedUserField;
-            const relatedKartoffelField: string | undefined = templateField.expandedUserField?.kartoffelField;
-
-            if (relatedUserField === relatedFieldKey && relatedKartoffelField)
-                entityProperties[fieldKey] = user[relatedKartoffelField as keyof IKartoffelUser] ?? entityProperties[fieldKey];
-        }
-    };
 
     for (const [key, field] of Object.entries(templateProperties)) {
         const fieldValue = entityProperties[key];
@@ -49,15 +58,15 @@ export async function handleUserFields(
 
         try {
             if (field?.format === 'user') {
-                const user: IKartoffelUser = normalizeUser(fieldValue, usersMap, key, [fieldValue]);
-                updateKartoffelFields(user, key);
+                const user = normalizeUser(fieldValue, usersMap, key, [fieldValue]);
+                updateKartoffelFields(user, key, templateProperties, entityProperties);
 
                 entityProperties[key] = JSON.stringify(await UsersManager.kartoffelUserToUser(user));
                 continue;
             }
             if (field?.type === 'array' && field?.items?.format === 'user') {
                 const users: string[] = [];
-                for (const identityCard of fieldValue as string[]) {
+                for (const identityCard of fieldValue) {
                     const kartoffelUser = normalizeUser(identityCard, usersMap, key, fieldValue);
                     const user = await UsersManager.kartoffelUserToUser(kartoffelUser);
                     users.push(JSON.stringify(user));
@@ -72,4 +81,4 @@ export async function handleUserFields(
     }
 
     if (errors.length) throw new AggregateError(errors, 'some user fields were not found');
-}
+};
