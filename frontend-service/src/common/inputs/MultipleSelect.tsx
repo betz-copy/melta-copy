47fa9b07ded1ee8ba7f52672c72afcd71b8c28cb
Uri Handelzalts <urihandelzalts@gmail.com>
2026-01-11 09:05:13 +0200

    fix: blame mayan

--- frontend-service/src/common/inputs/MultipleSelect.tsx
+++ frontend-service/src/common/inputs/MultipleSelect.tsx
@@ -1,6 +1,5 @@
 import { Close, ExpandMore } from '@mui/icons-material';
 import { Autocomplete, Grid, MenuItem, TextField, TextFieldProps } from '@mui/material';
-import { WidgetProps } from '@react-awesome-query-builder/core';
 import { RJSFSchema } from '@rjsf/utils';
 import React from 'react';
 import { IPropertyValue } from '../../interfaces/entities';
@@ -24,7 +23,7 @@ const MultipleSelect: React.FC<{
     onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
     variant: 'standard' | 'outlined';
     rawErrors?: string[];
-    textFieldProps?: Partial<WidgetProps>;
+    textFieldProps?: Partial<TextFieldProps>;
     value?: IPropertyValue;
     multiple?: boolean;
     disabled?: boolean;
