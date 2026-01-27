b39239543b79a48c2e7adb0e3f375b1a3a25a470
nadav882 <nadybeladev@gmail.com>
2026-01-27 13:15:42 +0200

    format (): fix the biome errors

--- frontend-service/src/MeltaUpdates.tsx
+++ frontend-service/src/MeltaUpdates.tsx
@@ -1,175 +1,142 @@
-import { CheckCircleOutline, InfoOutlined } from "@mui/icons-material";
-import {
-	Box,
-	Button,
-	Dialog,
-	DialogActions,
-	DialogContent,
-	Grid,
-	Typography,
-	useTheme,
-} from "@mui/material";
-import i18next from "i18next";
-import type React from "react";
-import Confetti from "react-confetti";
-import MeltaTooltip from "./common/MeltaDesigns/MeltaTooltip";
-import type { MeltaUpdatesConfig } from "./services/backendConfigService";
-import { useDarkModeStore } from "./stores/darkMode";
+import { CheckCircleOutline, InfoOutlined } from '@mui/icons-material';
+import { Box, Button, Dialog, DialogActions, DialogContent, Grid, Typography, useTheme } from '@mui/material';
+import i18next from 'i18next';
+import type React from 'react';
+import Confetti from 'react-confetti';
+import MeltaTooltip from './common/MeltaDesigns/MeltaTooltip';
+import type { MeltaUpdatesConfig } from './services/backendConfigService';
+import { useDarkModeStore } from './stores/darkMode';
 
 interface MeltaUpdatesProps {
-	open: boolean;
-	handleClose: () => void;
-	meltaUpdates: MeltaUpdatesConfig;
+    open: boolean;
+    handleClose: () => void;
+    meltaUpdates: MeltaUpdatesConfig;
 }
 
-const MeltaUpdates: React.FC<MeltaUpdatesProps> = ({
-	open,
-	handleClose,
-	meltaUpdates: { details, description },
-}) => {
-	const darkMode = useDarkModeStore((state) => state.darkMode);
-	const theme = useTheme();
+const MeltaUpdates: React.FC<MeltaUpdatesProps> = ({ open, handleClose, meltaUpdates: { details, description } }) => {
+    const darkMode = useDarkModeStore((state) => state.darkMode);
+    const theme = useTheme();
 
-	const circleDesign = {
-		display: "flex",
-		justifyContent: "center",
-		alignItems: "center",
-		borderRadius: "50%",
-	};
+    const circleDesign = {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '50%',
+    };
 
-	return (
-		<>
-			{open && <Confetti />}
+    return (
+        <>
+            {open && <Confetti />}
 
-			<Dialog
-				open={open}
-				onClose={handleClose}
-				slotProps={{
-					paper: {
-						sx: {
-							overflow: "visible",
-						},
-					},
-				}}
-			>
-				<Box
-					component="img"
-					src="/icons/melta-updates.svg"
-					sx={{
-						position: "absolute",
-						width: 170,
-						zIndex: 2000,
-						pointerEvents: "none",
-						top: "-24px",
-						left: "-17px",
-					}}
-				/>
-				<DialogContent
-					sx={{
-						minWidth: "480px",
-						display: "flex",
-						flexDirection: "column",
-						position: "relative",
-					}}
-				>
-					<Grid
-						display="flex"
-						flexDirection="column"
-						alignItems="center"
-						my={2}
-					>
-						<Grid marginBottom={1}>
-							<Box
-								sx={{
-									...circleDesign,
-									width: "48px",
-									height: "48px",
-									backgroundColor: darkMode ? "rgb(147, 152, 194)" : "#F1F7FF",
-								}}
-							>
-								<Box
-									sx={{
-										...circleDesign,
-										width: "35px",
-										height: "35px",
-										backgroundColor: darkMode ? "rgb(134 141 199)" : "#DEEDFF",
-									}}
-								>
-									<CheckCircleOutline
-										sx={{
-											color: darkMode ? "white" : theme.palette.primary.main,
-											fontSize: "16px",
-										}}
-									/>
-								</Box>
-							</Box>
-						</Grid>
-						<Typography
-							color={darkMode ? "#787c9e" : "#53566E"}
-							fontWeight={600}
-							fontSize="22px"
-						>
-							{i18next.t("meltaUpdates.title")}
-						</Typography>
-						{description && (
-							<Typography
-								color={darkMode ? "white" : "#53566E"}
-								fontSize="18px"
-							>
-								{description}
-							</Typography>
-						)}
-					</Grid>
+            <Dialog
+                open={open}
+                onClose={handleClose}
+                slotProps={{
+                    paper: {
+                        sx: {
+                            overflow: 'visible',
+                        },
+                    },
+                }}
+            >
+                <Box
+                    component="img"
+                    src="/icons/melta-updates.svg"
+                    sx={{
+                        position: 'absolute',
+                        width: 170,
+                        zIndex: 2000,
+                        pointerEvents: 'none',
+                        top: '-24px',
+                        left: '-17px',
+                    }}
+                />
+                <DialogContent
+                    sx={{
+                        minWidth: '480px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        position: 'relative',
+                    }}
+                >
+                    <Grid display="flex" flexDirection="column" alignItems="center" my={2}>
+                        <Grid marginBottom={1}>
+                            <Box
+                                sx={{
+                                    ...circleDesign,
+                                    width: '48px',
+                                    height: '48px',
+                                    backgroundColor: darkMode ? 'rgb(147, 152, 194)' : '#F1F7FF',
+                                }}
+                            >
+                                <Box
+                                    sx={{
+                                        ...circleDesign,
+                                        width: '35px',
+                                        height: '35px',
+                                        backgroundColor: darkMode ? 'rgb(134 141 199)' : '#DEEDFF',
+                                    }}
+                                >
+                                    <CheckCircleOutline
+                                        sx={{
+                                            color: darkMode ? 'white' : theme.palette.primary.main,
+                                            fontSize: '16px',
+                                        }}
+                                    />
+                                </Box>
+                            </Box>
+                        </Grid>
+                        <Typography color={darkMode ? '#787c9e' : '#53566E'} fontWeight={600} fontSize="22px">
+                            {i18next.t('meltaUpdates.title')}
+                        </Typography>
+                        {description && (
+                            <Typography color={darkMode ? 'white' : '#53566E'} fontSize="18px">
+                                {description}
+                            </Typography>
+                        )}
+                    </Grid>
 
-					{details &&
-						Object.entries(details).map(([title, description], index) => (
-							<Box
-								display="flex"
-								alignItems="center"
-								key={description}
-								gap={2}
-								margin={1}
-							>
-								<Box
-									sx={{
-										...circleDesign,
-										background: theme.palette.primary.main,
-										height: "35px",
-										width: "35px",
-									}}
-								>
-									<Typography fontWeight={700} fontSize="15px" color="white">
-										{index + 1}
-									</Typography>
-								</Box>
-								<Typography fontSize="16px" color={darkMode ? "" : "#53566E"}>
-									{title}
-								</Typography>
-								<MeltaTooltip title={description}>
-									<InfoOutlined sx={{ color: "#166BD4" }} />
-								</MeltaTooltip>
-							</Box>
-						))}
-				</DialogContent>
+                    {details &&
+                        Object.entries(details).map(([title, description], index) => (
+                            <Box display="flex" alignItems="center" key={description} gap={2} margin={1}>
+                                <Box
+                                    sx={{
+                                        ...circleDesign,
+                                        background: theme.palette.primary.main,
+                                        height: '35px',
+                                        width: '35px',
+                                    }}
+                                >
+                                    <Typography fontWeight={700} fontSize="15px" color="white">
+                                        {index + 1}
+                                    </Typography>
+                                </Box>
+                                <Typography fontSize="16px" color={darkMode ? '' : '#53566E'}>
+                                    {title}
+                                </Typography>
+                                <MeltaTooltip title={description}>
+                                    <InfoOutlined sx={{ color: '#166BD4' }} />
+                                </MeltaTooltip>
+                            </Box>
+                        ))}
+                </DialogContent>
 
-				<DialogActions>
-					<Button
-						variant="contained"
-						fullWidth
-						sx={{
-							margin: "15px",
-							borderRadius: "7px",
-						}}
-						onClick={handleClose}
-					>
-						<Typography color="white">
-							{i18next.t("meltaUpdates.confirmation")}
-						</Typography>
-					</Button>
-				</DialogActions>
-			</Dialog>
-		</>
-	);
+                <DialogActions>
+                    <Button
+                        variant="contained"
+                        fullWidth
+                        sx={{
+                            margin: '15px',
+                            borderRadius: '7px',
+                        }}
+                        onClick={handleClose}
+                    >
+                        <Typography color="white">{i18next.t('meltaUpdates.confirmation')}</Typography>
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </>
+    );
 };
 
 export { MeltaUpdates };
