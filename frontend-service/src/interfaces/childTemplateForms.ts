fc26603e3e8fea0e0f905060f6b8f478fcc0bfd8
Shay Maslaton <shaymaslaton@gmail.com>
2026-02-04 13:01:38 +0200

    fix: make ci works after merge with develop

--- frontend-service/src/interfaces/childTemplateForms.ts
+++ frontend-service/src/interfaces/childTemplateForms.ts
@@ -1,4 +1,4 @@
-import type { ByCurrentDefaultValue, IChildTemplatePopulatedFromDb, IChildTemplateProperty } from '@packages/child-template';
+import type { ByCurrentDefaultValue, IChildTemplatePopulated, IChildTemplateProperty } from '@packages/child-template';
 import type { RelativeDateFilters } from '@packages/rule-breach';
 import type { IAgGridFilter } from '../common/wizards/entityTemplate/commonInterfaces';
 
@@ -19,7 +19,7 @@ export interface IFieldChip {
 
 export type IChildTemplateFormProperty = Omit<IChildTemplateProperty, 'filters'> & { filters?: IAgGridFilter[] };
 
-export interface IChildTemplateForm extends Omit<IChildTemplatePopulatedFromDb, 'properties'> {
+export interface IChildTemplateForm extends Omit<IChildTemplatePopulated, 'properties'> {
     properties: { properties: Record<string, IChildTemplateFormProperty> };
 }
 
