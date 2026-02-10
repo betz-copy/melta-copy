f56f37b93003665d5c37dc0505d0b87f2b225fbe
Shay Maslaton <shaymaslaton@gmail.com>
2026-02-10 15:51:52 +0200

    fix: merge

--- semantic-service/src/express/semantics/controller.ts
+++ semantic-service/src/express/semantics/controller.ts
@@ -1,9 +1,10 @@
 import { Request, Response } from 'express';
+import { File } from 'multer';
 import SemanticManager from './manager';
 
 class SemanticController {
     static async summarize(req: Request, res: Response) {
-        const files = req.files as Express.Multer.File[];
+        const files = req.files as File[];
         const maxLength = req.body?.maxLength ? parseInt(req.body.maxLength, 10) : 50;
 
         const result = await SemanticManager.summarizeFiles(files, maxLength);
