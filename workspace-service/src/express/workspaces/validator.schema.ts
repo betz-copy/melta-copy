b9e12debe02b4f6813c846286595c5b52c04507c
Shay Maslaton <shaymaslaton@gmail.com>
2026-02-16 16:29:05 +0200

    fix: joi

--- workspace-service/src/express/workspaces/validator.schema.ts
+++ workspace-service/src/express/workspaces/validator.schema.ts
@@ -47,9 +47,7 @@ const metadataSchema = Joi.object({
     numOfRelationshipFieldsToShow: Joi.number(),
     numOfPreviewFieldsToShow: Joi.number(),
     twinTemplates: Joi.array().items(Joi.string().trim()).optional(),
-    aiSummary: Joi.object({
-        enabled: Joi.boolean(),
-    }).optional(),
+    enableAiSummary: Joi.boolean().optional(),
 }).optional();
 
 // Joi schema for Workspace
