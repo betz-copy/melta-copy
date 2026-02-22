8e61c3e99244a05e55054de1677d372053823a9c
Uri Handelzalts <urihandelzalts@gmail.com>
2026-02-22 13:01:07 +0200

    chore(testing): add documentation.

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,9 @@
 import { Config } from '@jest/types';
 import { globSync } from 'glob';
 
+// This file is temporary until we finish migrating all services (and fix their tests)
+// One all services migrated, we can remove this file and change pnpm run test to run testing w/ turbo
+
 const projects = globSync('*/jest.config.ts', {
     cwd: __dirname,
     ignore: ['node_modules/**', 'dist/**'],
